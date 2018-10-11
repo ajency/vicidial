88621c71d016fb30dae77ad8da2139adbf2444cf
Prasad Honvar <prasad@ajency.in>
2018-10-11 17:26:31 +0530

    reorganized functions and bug fixes

--- app/Elastic/ElasticQuery.php
+++ app/Elastic/ElasticQuery.php
@@ -6,31 +6,49 @@ use Elasticsearch\ClientBuilder;
 class ElasticQuery {
 	protected $params = ["index" => ""];
 
+	public function __construct()
+    {
+        $this->elastic_client = ClientBuilder::create()->build();
+    }
+
 	public function set_index(string $index){
 		$this->params ["index"] = $index;
 		return $this;
 	}
+
 	public function set_body(){
 		$this->params["body"] = [];
 		return $this;
 	}
 
-	public function reset_bool(){
-		if (isset($this->params['body'])){
-			$this->params['body']["query"]['bool'] = [];
-		}else{
+	public function set_query(){
+		if (!isset($this->params['body'])){
 			$this->set_body();
-			$this->params['body']["query"]['bool'] = [];
 		}
+		$this->params['body']["query"] = [];
 		return $this;
 	}
-	public function reset_must(){
-		if (isset($this->params['body']["query"]['bool'])){
-			$this->params['body']["query"]['bool']['must'] = [];
-		}else{
-			$this->reset_bool();
-			$this->params['body']["query"]['bool']['must'] = [];
+
+	public function reset_bool(){
+		if (!isset($this->params['body']["query"])){
+			$this->set_query();
 		}
+		$this->params['body']["query"]['bool'] = [];
+		return $this;
+	}
+
+	public function reset_must(){
+		if (!isset($this->params['body']["query"]['bool'])){
+			$this->reset_bool();
+		}
+		$this->params['body']["query"]['bool']['must'] = [];
+		return $this;
+	}
+
+	public function append_must($value){
+		if (!isset($this->params['body']["query"]['bool']['must']))
+			$this->reset_must();
+		$this->params['body']["query"]['bool']['must'][] = $value;
 		return $this;
 	}
 
@@ -38,16 +56,11 @@ class ElasticQuery {
 		return ["term"=>[$key=>$value]];
 	}
 
-	public function append_must($value){
-		if (!isset($this->params['body']["query"]['bool']['must'])) $this->reset_must();
-		$this->params['body']["query"]['bool']['must'][] = $value;
-		return $this;
-	}
-
 	public function search(){
 		$elastic_client = ClientBuilder::create()->build();
-		return $elastic_client->search($this->params);
+		return $this->elastic_client->search($this->params);
 	}
+
 	public function getParams(){
 		
 		return $this->params;
