9806c20bcbe37414c72cf90e52e27b2e4b20a60e
Prasad Honvar <prasad@ajency.in>
2018-10-11 17:17:39 +0530

    ElasticQuery class working

--- app/Elastic/ElasticQuery.php
+++ app/Elastic/ElasticQuery.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Elastic;
 
 use Elasticsearch\ClientBuilder;
@@ -7,27 +6,55 @@ use Elasticsearch\ClientBuilder;
 class ElasticQuery {
 	protected $params = ["index" => ""];
 
+	public function set_index(string $index){
+		$this->params ["index"] = $index;
+		return $this;
+	}
 	public function set_body(){
 		$this->params["body"] = [];
-		return true;
+		return $this;
 	}
 
 	public function reset_bool(){
 		if (isset($this->params['body'])){
-			$this->params['body']['bool'] = []
+			$this->params['body']["query"]['bool'] = [];
 		}else{
-			$this->reset_body();
-			$this->params['body']['bool'] = []
+			$this->set_body();
+			$this->params['body']["query"]['bool'] = [];
 		}
-		return true;
+		return $this;
 	}
-	public function reset_must_empty(){
-		if (isset($this->params['body']['bool'])){
-			$this->params['body']['bool']['must'] = []
+	public function reset_must(){
+		if (isset($this->params['body']["query"]['bool'])){
+			$this->params['body']["query"]['bool']['must'] = [];
 		}else{
-			$this-reset_bool();
-			$this->params['body']['bool']['must'] = []
+			$this->reset_bool();
+			$this->params['body']["query"]['bool']['must'] = [];
 		}
-		return true;
+		return $this;
+	}
+
+	public static function create_term($key,$value){
+		return ["term"=>[$key=>$value]];
+	}
+
+	public function append_must($value){
+		if (!isset($this->params['body']["query"]['bool']['must'])) $this->reset_must();
+		$this->params['body']["query"]['bool']['must'][] = $value;
+		return $this;
+	}
+
+	public function search(){
+		$elastic_client = ClientBuilder::create()->build();
+		return $elastic_client->search($this->params);
+	}
+	public function getParams(){
+		
+		return $this->params;
 	}		
 }
+
+
+#$els = new ElasticQuery
+#$product = ElasticQuery::create_term('product_id',$product_id);
+#$els->append_must($product);
