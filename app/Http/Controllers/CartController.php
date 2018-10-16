187707b4e58bbdd0397c0c3fedb3a0c1b881ff92
Prasad Honvar <prasad@ajency.in>
2018-10-16 10:22:13 +0530

    Code Formatter applied on Cart Controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,38 +9,44 @@ use Illuminate\Http\Request;
 class CartController extends Controller
 {
 
-    public function guest_get_count(Request $request){
-    	// \Log::info('enters guest_get_count function');
-    	$id = $request->session()->get('active_cart_id',false);
-    	// \Log::info('cart = '.$id);
-    	if($id){
-    		$cart = Cart::find($id);
-			return response()->json(['cart_count' => $cart->item_count()]);	
-    	}else return abort('404', "Cart not found for this session");
-    }
-
-    public function guest_add_item(Request $request){
-		// \Log::info('enters guest_get_count function');
-		$params = $request->all();
-    	$id = $request->session()->get('active_cart_id',false);
-    	$cart = ($id)? Cart::find($id) : new Cart;
-        $variant = Variant::where('odoo_id', $params['variant_id'])->first();
-        $item = $variant->getItemAttributes();
-    	if($item){
-    		$cart->insert_item(["id"=>$params['variant_id'], "quantity" =>$params['variant_quantity']]);
-    		// \Log::info($cart->cart_data);
-	    	$cart->save();
-	    	$message="Item added successfully";
-	    	$request->session()->put('active_cart_id',$cart->id);
-    	}
-    	return response()->json(['cart_count' => $cart->item_count(), "message" => $message, "item" => $item]);
-    	
-    }
-
-    public function guest_cart_fetch(Request $request){
+    public function guestGetCount(Request $request)
+    {
+        // \Log::info('enters guest_get_count function');
         $id = $request->session()->get('active_cart_id', false);
-        $cart = ($id)? Cart::find($id) : new Cart;
-        $items = [];
+        // \Log::info('cart = '.$id);
+        if ($id) {
+            $cart = Cart::find($id);
+            return response()->json(['cart_count' => $cart->item_count()]);
+        } else {
+            return abort('404', "Cart not found for this session");
+        }
+
+    }
+
+    public function guestAddItem(Request $request)
+    {
+        // \Log::info('enters guest_get_count function');
+        $params  = $request->all();
+        $id      = $request->session()->get('active_cart_id', false);
+        $cart    = ($id) ? Cart::find($id) : new Cart;
+        $variant = Variant::where('odoo_id', $params['variant_id'])->first();
+        $item    = $variant->getItemAttributes();
+        if ($item) {
+            $cart->insert_item(["id" => $params['variant_id'], "quantity" => $params['variant_quantity']]);
+            // \Log::info($cart->cart_data);
+            $cart->save();
+            $message = "Item added successfully";
+            $request->session()->put('active_cart_id', $cart->id);
+        }
+        return response()->json(['cart_count' => $cart->item_count(), "message" => $message, "item" => $item]);
+
+    }
+
+    public function guestCartFetch(Request $request)
+    {
+        $id          = $request->session()->get('active_cart_id', false);
+        $cart        = ($id) ? Cart::find($id) : new Cart;
+        $items       = [];
         $total_price = 0;
         foreach ($cart->cart_data as $cart_item) {
             $variant = Variant::where('odoo_id', $cart_item['id'])->first();
@@ -48,9 +54,9 @@ class CartController extends Controller
             $total_price += $variant->getPriceFinal();
         }
 
-        $cart = Cart::find($id);
-        $summary = ["total"=> $total_price, "discount" => 0, "tax" => "", "coupon" => ""];
-        $code = ["code" => "NEWUSER", "applied" => true];
-        return response()->json(['items' => $items, "summary" => $summary , "code" => $code]); 
+        $cart    = Cart::find($id);
+        $summary = ["total" => $total_price, "discount" => 0, "tax" => "", "coupon" => ""];
+        $code    = ["code" => "NEWUSER", "applied" => true];
+        return response()->json(['items' => $items, "summary" => $summary, "code" => $code]);
     }
 }
