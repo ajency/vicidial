b26b84532c64eb3c606d721bb47021b339a83fcb
Ajaj Rajguru <ajaj@ajency.in>
2018-10-22 15:38:51 +0530

    moved code to ready function and set radio checked event to false

--- public/js/productlisting.js
+++ public/js/productlisting.js
@@ -1,17 +1,23 @@
 
-$(".select-size input[type=radio]").change(function(evt){
-    if(this.dataset['list_price'] == this.dataset['sale_price']) {
-        jQuery('#kss-price-'+this.dataset['product_id']+'-'+this.dataset['color_id']).html('₹'+this.dataset['sale_price']);
-    } else {
-        jQuery('#kss-price-'+this.dataset['product_id']+'-'+this.dataset['color_id']).html('₹'+this.dataset['sale_price']+' <small class="kss-original-price text-muted">₹'+this.dataset['list_price']+'</small><span class="kss-discount text-danger">'+this.dataset['discount_per']+'% OFF</span>');
-    }
+$(function(){
 
-    var buttn = $(this).closest('.select-size').find("button");
-    buttn.removeClass("disabled");
-    buttn.prop("disabled", false);
-    buttn.addClass("cd-add-cart");
-    buttn.addClass("align-items-center");
-    buttn.addClass("d-flex");
-    buttn.addClass("justify-content-center");
-    buttn.html('<i class="kss_icon bag-icon-fill icon-sm"></i> Add To Bag');
-});
+    $('.kss_sizes .radio-input').prop('checked',false);
+
+    $(".select-size input[type=radio]").change(function(evt){
+        if(this.dataset['list_price'] == this.dataset['sale_price']) {
+            jQuery('#kss-price-'+this.dataset['product_id']+'-'+this.dataset['color_id']).html('₹'+this.dataset['sale_price']);
+        } else {
+            jQuery('#kss-price-'+this.dataset['product_id']+'-'+this.dataset['color_id']).html('₹'+this.dataset['sale_price']+' <small class="kss-original-price text-muted">₹'+this.dataset['list_price']+'</small><span class="kss-discount text-danger">'+this.dataset['discount_per']+'% OFF</span>');
+        }
+
+        var buttn = $(this).closest('.select-size').find("button");
+        buttn.removeClass("disabled");
+        buttn.prop("disabled", false);
+        buttn.addClass("cd-add-cart");
+        buttn.addClass("align-items-center");
+        buttn.addClass("d-flex");
+        buttn.addClass("justify-content-center");
+        buttn.html('<i class="kss_icon bag-icon-fill icon-sm"></i> Add To Bag');
+    });
+
+})
