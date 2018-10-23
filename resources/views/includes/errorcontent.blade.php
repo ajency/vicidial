d4783074ed3046ab778415fee90ad9a18a971605
Ajaj Rajguru <ajaj@ajency.in>
2018-10-23 14:53:44 +0530

    404 page content added

--- resources/views/includes/errorcontent.blade.php
+++ resources/views/includes/errorcontent.blade.php
@@ -1,1 +1,12 @@
-ERROR 404
+<div class="container mt-3">
+  <div class="row">
+      <div class="col-12 text-center my-4">
+            <img src="/img/empty_troly.png" class="img-fluid lazyload " width="400px" />
+            <h2 class="mt-4">We couldn't find any matches!</h2>
+            <p>Please check the spelling or try searching something else</p>
+            <a href="/">
+          		<button type="button" class="btn btn-outline-dark btn-lg m-auto d-block">Back to Home</button>
+        	</a>
+      </div>
+    </div>
+</div>
