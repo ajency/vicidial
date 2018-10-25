7610001c3821811f2a43a0775f1523ac05a9a862
Ajaj Rajguru <ajaj@ajency.in>
2018-10-25 11:39:24 +0530

    detach for color options in mobile

--- public/js/singleproduct.js
+++ public/js/singleproduct.js
@@ -3,20 +3,31 @@
     jQuery('.breadcrumb-item').last().addClass('active');
 });*/
 
-$('input[type=radio][name=kss-sizes]').change(function() {
-    //var selected_size_id = this.id;
-    //var selected_var_id = window.variants['selected_item'];
+$(function(){
+    $('input[type=radio][name=kss-sizes]').change(function() {
+        //var selected_size_id = this.id;
+        //var selected_var_id = window.variants['selected_item'];
 
-    //console.log(_.find(window.variants['items'][selected_var_id]['items'], function (o) { return o.size.id = selected_size_id; }));
+        //console.log(_.find(window.variants['items'][selected_var_id]['items'], function (o) { return o.size.id = selected_size_id; }));
 
-    if(this.dataset['list_price'] == this.dataset['sale_price']) {
-    	jQuery('#kss-price').html('₹'+this.dataset['sale_price']);
-    } else {
-    	jQuery('#kss-price').html('₹'+this.dataset['sale_price']+' <small class="kss-original-price text-muted">₹'+this.dataset['list_price']+'</small> <span class="kss-discount text-danger">'+this.dataset['discount_per']+'% OFF</span>');
+        if(this.dataset['list_price'] == this.dataset['sale_price']) {
+            jQuery('#kss-price').html('₹'+this.dataset['sale_price']);
+        } else {
+            jQuery('#kss-price').html('₹'+this.dataset['sale_price']+' <small class="kss-original-price text-muted">₹'+this.dataset['list_price']+'</small> <span class="kss-discount text-danger">'+this.dataset['discount_per']+'% OFF</span>');
+        }
+
+        replaceURLParameter('size', this.dataset['title']);
+
+        jQuery('#cd-add-to-cart').prop("disabled", false);
+
+    });   
+    
+    // Detaching color option in mobile
+    if ($(window).width() < 767) {
+        var coloroption = $('.colorOptions').detach();
+        $('.kss_sizes').after(coloroption);
+        $('.colorOptions').removeClass('d-none');
     }
 
-    replaceURLParameter('size', this.dataset['title']);
+})
 
-    jQuery('#cd-add-to-cart').prop("disabled", false);
-
-});
