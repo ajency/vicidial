878a953ea67bd4e4ccedbf2bce9e85e9ab03c4a3
Viraj Verlekar <viraj@ajency.in>
2018-10-29 11:36:49 +0530

    Order & SubOrder model

---
+++ database/migrations/2018_10_29_055220_create_sub_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->json('item_data');
+            $table->json('odoo_info')->nullable();
+            $table->integer('odoo_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_orders');
+    }
+}
