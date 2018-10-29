1c2c86cf4dd4d343cb914e54dee066e107dbbe1c
Ajaj Rajguru <ajaj@ajency.in>
2018-10-29 18:15:34 +0530

    image responsive paths + placeholder

--- resources/views/includes/singleproduct/productimages.blade.php
+++ resources/views/includes/singleproduct/productimages.blade.php
@@ -4,7 +4,14 @@
 
 		<img src="/img/arrow.png" class="swipe-arrow  d-block d-md-none">
 		<div class="loader"></div>
-		<ul id="aniimated-thumbnials" class="d-flex d-sm-block list-unstyled m-0 m-md-2 prod-slides hidden">
+			@php  if(count((array)$params['images'])>0) {
+				$divclass = 'prod-slides';
+			}
+			else {
+				$divclass = 'slider-placeholder-img';
+			}
+			@endphp
+		<ul id="aniimated-thumbnials" class="d-flex d-sm-block list-unstyled m-0 m-md-2 {{$divclass}} hidden">
 
 			@php
 			if(count((array)$params['images'])>0) {
@@ -12,23 +19,21 @@
 					$image_1x = $image_set->{'main'}->{'1x'};
 					$image_2x = $image_set->{'main'}->{'2x'};
 					$image_3x = $image_set->{'main'}->{'3x'};
+					$image_zoom = $image_set->{'zoom'}->{'1x'};
+					$image_thumb = $image_set->{'thumb'}->{'1x'};
 			@endphp
-					<li class="mx-2 mb-2" >
-						<a href="{{$image_3x}}" class="custom-selector">
-							<img data-src="{{$image_1x}}" class="lazyload"  data-srcset="{{$image_1x}} 1x, {{$image_2x}} 2x" >
-
-						</a>
-					</li>
+				<li class="mx-2 mb-2" >
+					<a href="{{$image_zoom}}" class="custom-selector">
+						<img data-src="{{$image_thumb}}" class="lazyload" data-srcset="{{$image_1x}} 326w, {{$image_2x}} 652w, {{$image_3x}} 978w" sizes="(min-width: 768px) 50vw">
+					</a>
+				</li>
 			@php
-					}
+			}
 			} else  {
 			@endphp
-			<li class="mx-2 mb-2" >
-				<a href="/img/3x/4front@3x.jpg" class="custom-selector">
-					<img data-src="/img/1x/4front@1x.jpg" class="lazyload"  data-srcset="/img/1x/4front@1x.jpg 1x, /img/2x/4front@2x.jpg 2x" >
-
-				</a>
-			</li>
+				<li class="mb-3 w-100">
+					<img src="/img/placeholder.svg" class="lazyload" >
+				</li>
 			@php
 			}
 			@endphp
