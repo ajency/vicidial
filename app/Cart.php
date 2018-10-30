14143d1673601922ac82293c93fde89935f4e5d8
Viraj Verlekar <viraj@ajency.in>
2018-10-30 15:35:34 +0530

    cart qty string to int

--- app/Cart.php
+++ app/Cart.php
@@ -29,7 +29,7 @@ class Cart extends Model
         if (valInteger($item, self::ITEM_FIELDS)) {
             $item                   = array_only($item, self::ITEM_FIELDS);
             $cart_data              = $this->cart_data;
-            $cart_data[$item["id"]] = ["id" => $item["id"], "quantity" => $item["quantity"], 'timestamp' => Carbon::now()->timestamp];
+            $cart_data[$item["id"]] = ["id" => $item["id"], "quantity" => intval($item["quantity"]), 'timestamp' => Carbon::now()->timestamp];
             $this->cart_data        = $cart_data;
             // \Log::info($this->cart_data);
         } else {
