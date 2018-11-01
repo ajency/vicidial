f971e480d9045b959401695ffbc1ac762b445a2e
tanvi salkar <tanvi.s@ajency.in>
2018-11-01 10:55:36 +0530

    no products view

---
+++ resources/views/noproducts.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+
+@section('content')
+    
+	<div class="container mt-3">
+	  <div class="row">
+	      <div class="col-12 text-center my-4">
+	            <img src="/img/empty_troly.png" class="img-fluid lazyload " width="400px" />
+	            <h2 class="mt-4">no products available!</h2>
+	            <p>Please check the spelling or try searching something else</p>
+	            <a href="/">
+	          		<button type="button" class="btn btn-outline-dark btn-lg m-auto d-block">Back to Home</button>
+	        	</a>
+	      </div>
+	    </div>
+	</div>
+
+@stop
