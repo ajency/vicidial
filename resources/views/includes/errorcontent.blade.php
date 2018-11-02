b4480f955a6bf5f4430a188d6fbd83e63a7f059f
Ajaj Rajguru <ajaj@ajency.in>
2018-11-02 12:52:01 +0530

    404 page updated

--- resources/views/includes/errorcontent.blade.php
+++ resources/views/includes/errorcontent.blade.php
@@ -1,9 +1,9 @@
 <div class="container mt-3">
   <div class="row">
       <div class="col-12 text-center my-4">
-            <img src="/img/empty_troly.png" class="img-fluid lazyload " width="400px" />
-            <h2 class="mt-4">We couldn't find any matches!</h2>
-            <p>Please check the spelling or try searching something else</p>
+            <img src="/img/404.png" class="img-fluid lazyload " width="400px" />
+            <h2 class="mt-4">Page not found!</h2>
+            <p class="my-3">Please check the spelling or try searching something else</p>
             <a href="/">
           		<button type="button" class="btn btn-outline-dark btn-lg m-auto d-block">Back to Home</button>
         	</a>
