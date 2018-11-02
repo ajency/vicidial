5953efc8abf320805762e71838e8abb96737bec2
Ajaj Rajguru <ajaj@ajency.in>
2018-11-02 17:51:49 +0530

    failure order details message updated

--- resources/views/includes/orderdetails/ordermessage.blade.php
+++ resources/views/includes/orderdetails/ordermessage.blade.php
@@ -6,19 +6,19 @@
 	</button>
 	@if($status == 'success')
 	<div>
-	<h4 class="font-weight-bold text-success mb-3"><i class="fas fa-check-circle"></i> Order Confirmed</h4>
-	<h4 class="font-weight-bold"> Thank you for shopping at kidsuperstore.in</h4>
-	<p>Your order has been placed and is being processed, When the item are shipped you will receive an email with the details </p>
-	Easily track all Kidsuperstore Orders!
+		<h4 class="font-weight-bold text-success mb-3"><i class="fas fa-check-circle"></i> Order Confirmed</h4>
+		<h4 class="font-weight-bold"> Thank you for shopping at kidsuperstore.in</h4>
+		<p>Your order has been placed and is being processed, When the item are shipped you will receive an email with the details </p>
+		Easily track all Kidsuperstore Orders!
 	</div>
 	@endif
 	@if($status == 'failure')
 	<div>
-	<h4 class="font-weight-bold">Payment Failed</h4>
-	<p>We could not process your payment. This could be due to the following reasons :</p>
-	1. The CVV or Expiry Date might be wrong
-	2. Your banck network might be down
-	3. You have cancelled the transaction
+		<h4 class="font-weight-bold"><i class="fas fa-times-circle"></i> Payment Failed</h4>
+		<p class="mb-2">We could not process your payment. This could be due to the following reasons :</p>
+		1. The CVV or Expiry Date might be wrong<br>
+		2. Your banck network might be down<br>
+		3. You have cancelled the transaction
 	</div>
 	@endif
 </div>
