928cc20ff3849d95c7611db07d2156393d26903d
jeromie <jeromie@ajency.in>
2018-11-05 15:26:36 +0530

    resources moved, mix setup

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -1,5 +1,5 @@
 <noscript id="deferred-styles">
-	<link rel="stylesheet" type="text/css" href="/css/combine.css">
+	<link rel="stylesheet" type="text/css" href="{{ mix('/css/all.css') }}">
 </noscript>
 <script>
   var loadDeferredStyles = function() {
@@ -27,9 +27,6 @@
 	lazySizesConfig.loadMode = 3;
 </script>
 
-<script type="text/javascript" src="/js/combine.js"></script>
-
-
-<script type="text/javascript" src="/js/custom.js"></script>
+<script type="text/javascript" src="{{ mix('/js/all.js') }}"></script>
 
 <script type="text/javascript" src="/js/cart.js"></script>
