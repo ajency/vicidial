4ea615277e6a094f0676489984374664dd4dd93a
Shashank Shetye Saudagar <shashank@ajency.in>
2018-11-10 16:09:50 +0530

    Odoo id created while saving the user

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Cart;
+use App\Elastic\OdooConnect;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -57,20 +58,60 @@ class User extends Authenticatable
     public function newCart($replicate = false)
     {
         // $cart = Cart::create(['user_id' => $this->id, 'active' => 1, 'type' => 'cart']);
-        $cart = new Cart;
+        $cart          = new Cart;
         $cart->user_id = $this->id;
         $cart->save();
         $ac = $this->activeCart();
-        if($replicate){
+        if ($replicate) {
             $cart->cart_data = $ac->cart_data;
             $cart->save();
         }
         $ac->active = 0;
         $ac->save();
-        if($ac->type == 'cart' && empty($ac->cart_data)){
+        if ($ac->type == 'cart' && empty($ac->cart_data)) {
             $ac->delete();
         }
         $this->cart_id = $cart->id;
         $this->save();
+    }
+
+    private function odooSync()
+    {
+        $odoo  = new OdooConnect;
+        $model = "res.partner";
+        if ($this->odoo_id == null) {
+            //check if the id with that phone number exists
+            $odoo_id = $odoo->defaultExec($model, 'search_read', [[ 
+                ['phone', '=', $this->phone],
+                ['customer', '=', true],
+                ['type', '=', 'contact'],
+            ]],
+                [
+                    "fields" => ['id'],
+                    'limit'  => 1,
+                ])->first()['id'];
+            if ($odoo_id == null) {
+                //if customer does not exist, create a new customer
+                $this->odoo_id = $odoo->defaultExec($model, 'create', [[
+                    "customer"   => true,
+                    "name"       => $this->name,
+                    "phone"      => $this->phone,
+                    "email"      => ($this->email) ? $this->email : "",
+                    "type"       => "contact",
+                    "is_company" => false,
+                ]], null)->first();
+            } else {
+                $this->odoo_id = $odoo_id;
+            }
+        }else{
+            //TODO
+            //update the customer if the customer already exists
+        }
+    }
+
+    public function save(array $options = [])
+    {
+        $this->odooSync();
+        parent::save($options);
     }
 }
