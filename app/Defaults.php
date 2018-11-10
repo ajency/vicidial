58fc30a9b91774c8266ec93799312ffed68181b6
Shashank Shetye Saudagar <shashank@ajency.in>
2018-11-10 19:32:58 +0530

    Command for getting states

--- app/Defaults.php
+++ app/Defaults.php
@@ -12,20 +12,21 @@ class Defaults extends Model
         'meta_data' => 'array',
     ];
 
-    public static function getStateList()
+    public static function getStatesFromOdoo()
     {
         $odoo   = new OdooConnect;
-        $states = $odoo->defaultExec('res.country.state', 'search_read', [[['country_id', '=', 104]]], ["limit" => 60, "fields" => ['name', 'code']]);
+        $states = $odoo->defaultExec('res.country.state', 'search_read', [[['country_id', '=', 104]]], ["limit" => 60, "fields" => ['name', 'code']])->reject(function ($value, $key) {return $value['name'] != trim($value['name']);});
         return $states;
     }
 
-    public static function saveStateList()
+    public static function saveOdooStates()
     {
-        foreach (self::getStateList() as $state) {
+        Defaults::where('type', 'state')->delete();
+        foreach (self::getStatesFromOdoo() as $state) {
             $defaults            = new Defaults;
             $defaults->type      = 'state';
             $defaults->label     = $state["name"];
-            $defaults->meta_data = ['name'=> $state["name"], 'code' => $state['code'], 'odoo_id' => $state["id"]];
+            $defaults->meta_data = ['name' => $state["name"], 'code' => $state['code'], 'odoo_id' => $state["id"]];
             $defaults->save();
         }
     }
