0ac4af11834346daa7f61afe6a3537957faba89e
Shashank Shetye Saudagar <shashank@ajency.in>
2018-11-12 16:50:16 +0530

    odoo default config for orders

--- config/orders.php
+++ config/orders.php
@@ -1,5 +1,11 @@
 <?php
 return [
-	"expiry" => env('ORDER_EXPIRY',15),
-	"payu_expiry" => env('PAYU_ORDER_EXPIRY',15),
+    "expiry"        => env('ORDER_EXPIRY', 15),
+    "payu_expiry"   => env('PAYU_ORDER_EXPIRY', 15),
+    "odoo_order_defaults" => [
+        
+    ],
+    "odoo_orderline_defaults" => [
+
+    ],
 ];
