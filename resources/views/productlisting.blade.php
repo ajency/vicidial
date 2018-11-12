05239a9639ede0aacc09453970b426f9c8786afc
Ajaj Rajguru <ajaj@ajency.in>
2018-11-12 14:10:56 +0530

    delaycss removed

--- resources/views/productlisting.blade.php
+++ resources/views/productlisting.blade.php
@@ -1,12 +1,7 @@
 @extends('layouts.default')
 
-@php
-  $delaycss = true;
-@endphp
-
 @section('headjs')
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.12/handlebars.min.js"></script>
-  @include('includes.abovethefold.productlistingcss')
 @stop
 
 @section('content')
