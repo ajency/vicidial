cc30c8388a86fdd98087f152779da72d72f3bf6e
Ajaj Rajguru <ajaj@ajency.in>
2018-11-12 19:21:49 +0530

    less conflicts resolved

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -29,24 +29,16 @@
    </div>
    <!-- Stores -->
    <div class="row d-none d-sm-block">
-      <div class="col-sm-12 text-center my-4">
-         <h3 class="text-gray">Our help center is designed to answer question instantly; if you cannot find answers there or have any comments or suggestions we welcome you to contact us directly.</h3>
+      <div class="col-sm-12 my-4">
+         <h3 class="line-height-5 font-weight-bold">Our help center is designed to answer question instantly; if you cannot find answers there or have any comments or suggestions we welcome you to contact us directly.</h3>
       </div>
    </div>
    <div class="row justify-content-center">
-      <div class="col-sm-12 order-last order-md-first mb-sm-3">
-         <div class="row mt-sm-4">
-            <div class="col-sm-12 mb-2 mb-sm-3 text-center">
-               <h4 class="font-weight-bold d-block mb-sm-3"><i class="far fa-envelope"></i> Send us an email</h4>
-               <p class="h5 kss-title">Message us directly and we aim to respond within 24hours. Our e-mail address is <a href="mailto:care@kidsuperstore.in">care@kidsuperstore.in</a></p>
-            </div>
-         </div>
-      </div>
       <!-- Contact form -->
-      <div class="col-sm-7 order-first order-md-last pb-3 pb-sm-5 pb-md-0 shadow contactForm no-shadow-mobile">
-         <div class="row p-md-4">
+      <div class="col-sm-8 pb-3 pb-sm-5 pb-md-0 contactForm">
+         <div class="row ml-0 p-md-4 mr-3 shadow no-shadow-mobile">
             <div class="col-md-12">
-               <h3 class="text-center mb-sm-3">Send us a message to let us know how we can help</h3>
+               <h4 class="text-center mb-sm-3 font-weight-bold">Send us a message to let us know how we can help</h4>
                <!--firstName-->
                <div class="form-group ">
                   <input class="form-control form-control-lg" id="names" type="text">
@@ -75,6 +67,15 @@
                <div class="form-group mt-3">
                   <button type="button" class="btn btn-primary btn-lg btn-block">Submit</button>
                </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-sm-4 mb-sm-3">
+         <div class="row m-0">
+            <div class="col-sm-12 shadow no-shadow-mobile p-sm-4 text-center">
+               <img src="/img/envelope.png" alt="message" width="100" class="mb-2 mt-3">
+               <h4 class="font-weight-bold d-block mb-4"> Send us an email</h4>
+               <p class="h5 line-height-5">Message us directly and we aim to respond within 24hours. Our e-mail address is <a href="mailto:care@kidsuperstore.in">care@kidsuperstore.in</a></p>
             </div>
          </div>
       </div>
