0211424ef85376a8a171bdc3f52b4131ae402839
Viraj Verlekar <viraj@ajency.in>
2018-11-16 16:28:33 +0530

    Testing data based on env

--- resources/views/includes/singleproduct/productsizes.blade.php
+++ resources/views/includes/singleproduct/productsizes.blade.php
@@ -10,7 +10,7 @@
     	@endphp
     	<input class="d-none radio-input" type="radio" name="kss-sizes" id="size-{{$size_set->size->id}}" {{$price_set['checked']}} data-variant_id="{{$size_set->id}}" {{$price_set['disabled']}} data-list_price="{{$price_set['list_price']}}" data-sale_price="{{$price_set['sale_price']}}" data-discount_per="{{$price_set['discount_per']}}" @php if($price_set['disabled']) { @endphp data-title="{{$size_set->size->name}}. Out of Stock" @php } else { @endphp data-title="{{$size_set->size->name}}" @php } @endphp />
 	    <label class="radio-label" for="size-{{$size_set->size->id}}" @php if($price_set['disabled']) { @endphp title="{{$size_set->size->name}}. Out of Stock" @php } else { @endphp title="{{$size_set->size->name}}" @php } @endphp>
-	      <div class="radio-option">{{$size_set->id}} : {{$size_set->size->name}}</div>
+	      <div class="radio-option"> @php if(config('app.env')!='production') echo $size_set->id." : "; @endphp {{$size_set->size->name}}</div>
 	    </label>
     	@php
      } @endphp
