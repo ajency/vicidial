d2d1b3af8f519baf73506fad2700b43342b7095d
Viraj Verlekar <viraj@ajency.in>
2018-11-16 17:07:00 +0530

    Inventory count based on location

--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    public function warehouse()
+    {
+        return $this->belongsTo('App\Warehouse', 'warehouse_odoo_id', 'odoo_id');
+    }
 }
