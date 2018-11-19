4effd674223882de9d2cb9884c1465f7bb513a0f
Shashank Shetye Saudagar <shashank@ajency.in>
2018-11-19 13:15:08 +0530

    Method to index all locations

--- config/odoo.php
+++ config/odoo.php
@@ -1,13 +1,32 @@
 <?php
 $connections = array();
-$conn_cnt = env('ODOO_CONN_CNT', '0');
-for ($conn=1; $conn <= intval($conn_cnt); $conn++) { 
-    $connections[] = array("username"=>env('ODOO_USER'.$conn, ''),"password"=>env('ODOO_PASS'.$conn, ''));
+$conn_cnt    = env('ODOO_CONN_CNT', '0');
+for ($conn = 1; $conn <= intval($conn_cnt); $conn++) {
+    $connections[] = array("username" => env('ODOO_USER' . $conn, ''), "password" => env('ODOO_PASS' . $conn, ''));
 }
 
 return [
-	'url' => env('ODOO_URL', ''),
-	'db' => env('ODOO_DB', ''),
-	'limit' => intval(env('ODOO_LIMIT', '')),
-    'connections' => $connections,
+    'url'          => env('ODOO_URL', ''),
+    'db'           => env('ODOO_DB', ''),
+    'limit'        => intval(env('ODOO_LIMIT', '')),
+    'connections'  => $connections,
+
+    'model_fields' => [
+        'location'   => [
+            'name',//
+            'company_id',//
+            "usage", //
+            "warehouse_id",// 
+            "location_id",// 
+            "display_name", //
+            "city", 
+            "state_name", 
+            "street", 
+            "street2", 
+            "zip",
+            "store_code",
+        ],
+        'warehouses' => [],
+        'states'     => [],
+    ],
 ];
--- database/migrations/2018_11_15_164917_create_locations_table.php
+++ database/migrations/2018_11_15_164917_create_locations_table.php
@@ -17,8 +17,17 @@ class CreateLocationsTable extends Migration
             $table->increments('id');
             $table->integer('odoo_id');
             $table->string('name');
-            $table->integer('warehouse_odoo_id');
+            $table->integer('warehouse_odoo_id')->nullable();
+            $table->string('warehouse_name')->nullable();
+            $table->integer('location_odoo_id')->nullable();
+            $table->string('location_name')->nullable();
             $table->string('display_name')->nullable();
+            $table->integer('company_odoo_id')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('type')->nullable();
+            $table->json('address')->nullable();
+            $table->string('store_code')->nullable();
+            $table->boolean('use_in_inventory')->default(0);
             $table->timestamps();
             $table->unique('odoo_id');
         });
