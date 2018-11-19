664d5d78ccec27a48c12f12fb5fcc03b4e5e570c
sujitgawas19 <sujit@ajency.in>
2018-11-19 17:55:36 +0530

    test route for checking email template

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -109,5 +109,10 @@ class OrderController extends Controller
 
         return view('orderdetails')->with('params', $params);
     }
+
+    public function testEmail()
+    {
+        return view('/email/order-success');
+    }
 }
 
