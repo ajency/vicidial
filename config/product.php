cafbf764876f7609f81beffd44abd76f04e2ffe8
tanvi salkar <tanvi.s@ajency.in>
2018-11-20 12:25:14 +0530

    config change for price filter bucket range done

--- config/product.php
+++ config/product.php
@@ -254,5 +254,6 @@ return [
     ], //Used for breadcrumbs on single product page
     "list_page_display_limit" => 30,
     "similar_products_display_limit" => 5,
+    "price_filter_bucket_range" => ["min"=>0,"max"=>2000],
 
 ];
