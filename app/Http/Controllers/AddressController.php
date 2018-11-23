a9ddea13832608b2d1460ed1f25b08121ef6c220
Viraj Verlekar <viraj@ajency.in>
2018-11-23 12:52:17 +0530

    fetch address - check cart only if cart_id is set

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -61,13 +61,13 @@ class AddressController extends Controller
 
     public function userFetchAddresses(Request $request)
     {
-        $request->validate(['cart_id' => 'required|exists:carts,id']);
-
         $user = User::getUserByToken($request->header('Authorization'));
 
         $params  = $request->all();
-        $cart = Cart::find($params['cart_id']);
-        validateCart($user, $cart, 'cart');
+        if(isset($params['cart_id'])) {
+            $cart = Cart::find($params['cart_id']);
+            validateCart($user, $cart, 'cart');
+        }
 
         foreach ($cart->cart_data as $variant_id => $variant_details) {
             $variant = Variant::find($variant_id);
