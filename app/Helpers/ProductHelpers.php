7e21cdc900b1a003ed008cee7abd5c3a7632dacd
Prasad Honvar <prasad@ajency.in>
2018-11-27 18:45:06 +0530

    LHS availability checkbox fix

--- app/Helpers/ProductHelpers.php
+++ app/Helpers/ProductHelpers.php
@@ -210,7 +210,7 @@ function sanitiseFilterdata($result, $params = [])
         [
             "display_name" => config('product.facet_display_data.variant_availability.item_display_name'),
             "facet_value"  => true,
-            "is_selected" => isset($params['search_object']['boolean_filter']['variant_availability']) && $params['search_object']['boolean_filter']['variant_availability'],
+            "is_selected" => (!isset($params['search_object']['boolean_filter']['variant_availability']) or $params['search_object']['boolean_filter']['variant_availability']),
             "count" => 20,
         ],
     ];
