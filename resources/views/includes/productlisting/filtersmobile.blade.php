aaceaacc4a1c5f7812125cb965187b034b10bca9
Ajaj Rajguru <ajaj@ajency.in>
2018-11-29 18:40:16 +0530

    Enabled sort filter in mobile

--- resources/views/includes/productlisting/filtersmobile.blade.php
+++ resources/views/includes/productlisting/filtersmobile.blade.php
@@ -32,13 +32,11 @@
 
     <div class="fixed-bottom d-block d-md-none">
         <div class="row no-gutters">
-            <!-- <div class="col-6 text-center b-r">
-              <button  data-toggle="modal" data-target="#kss_sort" type="button" class="btn btn-lg btn-block btn-link text-dark mt-1 mb-1"><i class="fa-sort-amount-down fas text-secondary"></i> Sort</button>
-  
-            </div> -->
-            <div class="col-12 text-center">
-                <!-- <button id="filter" type="button" class="kss-btn kss-btn--fab"><i class="fa-filter fas"></i></button> -->
-                <button id="filter" type="button" class="btn btn-lg btn-block btn-primary text-black p-2 text-black text-uppercase w-100" style="padding: 0.8em !important;width:100%;">Filter</button>
+            <div class="col-6 text-center b-r">
+                <button  data-toggle="modal" data-target="#kss_sort" type="button" class="btn btn-lg btn-block btn-link text-dark mt-1 mb-1"><i class="fa-sort-amount-down fas text-secondary"></i> Sort</button>
+            </div> 
+            <div class="col-6 text-center">
+                <button id="filter" type="button" class="btn btn-lg btn-block btn-primary text-black p-2 text-black text-uppercase w-100"><i class="fa-filter fas text-secondary"></i> Filter</button>
             </div>
         </div>
     </div>
