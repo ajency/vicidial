cc003e458287d61df413be91595e0d53642193bc
Viraj Verlekar <viraj@ajency.in>
2018-12-05 18:18:50 +0530

    product attribute fetch command

---
+++ app/Console/Commands/updateAttribute.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Facet;
+
+class updateAttribute extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'odoo:update_attribute {attribute}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add attribute to facets table';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $attribute  = strtoupper($this->argument('attribute'));
+        Facet::updateAttribute($attribute);
+    }
+}
--- app/Facet.php
+++ app/Facet.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Elastic\OdooConnect;
 
 class Facet extends Model
 {
@@ -10,4 +11,33 @@ class Facet extends Model
         'display' => 'boolean',
     ];
 	
+	public static function updateAttribute($attribute)
+    {
+        $offset	= 0;
+        $facets = ['COLOR' => 'product_color_html', 'SIZE' => 'variant_size_name'];
+        if(!isset($facets[$attribute])) $facets[$attribute] = 'variant_'.strtolower($attribute).'_name';
+        do {
+            $odoo           = new OdooConnect;
+	        $attributesData = $odoo->defaultExec("product.attribute.value", 'search_read', [[["id", ">", 0 ]]], ['order' => 'id', 'offset' => $offset]);
+
+            foreach ($attributesData as $attributeData) {
+                if($attributeData['attribute_id'][1] == $attribute) {
+                    try {
+                        $facetObj               = new Facet;
+                        $facetObj->facet_name   = $facets[$attribute];
+                        $facetObj->facet_value  = ($facets[$attribute] == 'product_color_html') ? $attributeData['html_color'] : $attributeData['name'];
+                        $facetObj->display_name = $attributeData['name'];
+                        $facetObj->slug         = ($facets[$attribute] == 'product_color_html') ? str_slug($attributeData['html_color']) : str_slug($attributeData['name']);
+                        $facetObj->sequence     = 10000;
+                        $facetObj->display      = (count($attributeData['product_ids'])>0) ? true : false;
+                        $facetObj->save();
+                    } catch (\Exception $e) {
+                        \Log::warning($e->getMessage());
+                    }
+                }
+            }
+
+            $offset 		= $offset + $attributesData->count();
+        } while ($attributesData->count() == config('odoo.limit'));
+    }
 }
