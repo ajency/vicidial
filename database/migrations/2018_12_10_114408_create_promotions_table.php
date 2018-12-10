05beab0d08fa7453b68fe9eb4b06070e0759d954
Viraj Verlekar <viraj@ajency.in>
2018-12-10 18:32:34 +0530

    Promotion Model

---
+++ database/migrations/2018_12_10_114408_create_promotions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type')->default('cart');
+            $table->integer('odoo_id');
+            $table->string('title');
+            $table->integer('value');
+            $table->string('discount_type');
+            $table->string('step_quantity');
+            $table->timestamp('start');
+            $table->timestamp('expire');
+            $table->string('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
