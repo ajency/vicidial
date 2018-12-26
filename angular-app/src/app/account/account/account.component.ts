1e24bfe384a8e8c576348fcb2ed086ee4ddf3468
sujitgawas19 <sujit@ajency.in>
2018-12-27 00:21:29 +0530

    reused pages and components for account app

---
+++ angular-app/src/app/account/account/account.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from '../../service/app-service.service';
+import { LoginComponentComponent } from '../../shared-components/login/login-component/login-component.component';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+
+	closeModalSubscription: Subscription;
+  openModalSubscription : Subscription;
+
+  constructor(private appservice : AppServiceService,
+  			  private router : Router) {
+  	        this.closeModalSubscription = this.appservice.listenToCloseModal().subscribe(()=>{ this.closeLoginModal()});
+	        this.openModalSubscription = this.appservice.listenToOpenModal().subscribe(()=>{ this.modalHandler()});
+  	}
+
+  ngOnInit() {
+  	this.appservice.removeLoader();
+  	if(!this.appservice.isLoggedInUser())
+  		this.displayModal();
+  }
+
+  ngOnDestroy(){
+    this.closeModalSubscription.unsubscribe();
+    this.openModalSubscription.unsubscribe();
+  }
+
+  isLoggedIn(){
+    return this.appservice.isLoggedInUser();
+  }
+
+  displayModal(){
+    let url = window.location.href +'/user-verification';
+    if(!window.location.href.endsWith('#/account/user-verification'))
+      history.pushState({cart : true}, 'cart', url);
+    $('#signin').modal('show');
+    $('.modal-backdrop').appendTo('#cd-my-account');
+    $('body').addClass('hide-scroll');
+  }
+
+  loginSucessTriggered(){
+  	history.back();
+  }
+
+   closeLoginModal(){   	
+    $('#signin').modal('hide');
+    $("#cd-my-account").css("overflow", "auto");
+
+    // To be added back once account page is ready
+    // if(this.appservice.redirectUrl.endsWith('/my-orders') && this.appservice.isLoggedInUser()){
+    // 	console.log("navigate to my-orders");
+  		// // this.router.navigateByUrl('account/my-orders');
+  		// this.router.navigate(['account/my-orders']);
+  		// this.appservice.redirectUrl = '';
+    // }
+
+    // Temporary fix for not letting user see account page on history back
+    history.back();
+  }
+
+  modalHandler(){
+    if(!this.appservice.isLoggedInUser())
+      this.displayModal();    
+  }
+
+  closeWidget(){
+  	let url = window.location.href.split("#")[0];
+    history.replaceState({}, 'account', url);
+    this.appservice.closeWidget();
+    window.location.reload();
+  }
+
+}
---
+++ angular-app/src/app/account/my-orders/my-orders.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppServiceService } from '../../service/app-service.service';
+import { ApiServiceService } from '../../service/api-service.service';
+
+import { BagSummaryComponent } from '../../shared-components/bag-summary/bag-summary/bag-summary.component';
+import { OrderComponent } from '../components/order/order.component';
+// import { LoginComponentComponent } from '../../../shared-components/login/login-component/login-component.component';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-my-orders',
+  templateUrl: './my-orders.component.html',
+  styleUrls: ['./my-orders.component.css']
+})
+export class MyOrdersComponent implements OnInit {
+
+	orders : any = [];
+  order_params = { page : 1, display_limit : 10 }
+  displayShowMore : boolean = true;
+  apiCallComplete : boolean = false;
+
+  constructor(private router: Router,
+						private appservice : AppServiceService,
+						private apiservice : ApiServiceService,) { 
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit my-orders");
+  	// this.appservice.removeLoader();
+    if(this.appservice.isLoggedInUser()){
+    	this.getOrders();      
+    }
+    else
+      this.appservice.removeLoader();
+
+  }
+
+  navigateToBlank(){
+  	this.router.navigateByUrl('/blank');
+  }
+
+  getOrders(){
+    if(this.appservice.myOrders){
+      this.orders = this.appservice.myOrders;
+      this.apiCallComplete = true;
+    }
+    else{
+      this.appservice.showLoader();
+     
+      this.appservice.getOrders().then((response)=>{
+        let formatted_data = this.appservice.formattedCartDataForUI(response.data);
+        this.orders = formatted_data;
+        this.appservice.myOrders = this.orders;
+        console.log("orders ==>", this.orders);
+        this.appservice.removeLoader();
+        this.apiCallComplete = true;
+      })
+      .catch((error)=>{
+        console.log("error ===>", error);
+        this.appservice.removeLoader();
+      }) 
+    }
+  }
+
+  isLoggedIn(){
+    return this.appservice.isLoggedInUser();
+  }
+
+  closeWidget(){
+    let url = window.location.href.split("#")[0];
+    history.replaceState({}, 'account', url);
+    this.appservice.closeCart();
+    // window.location.reload();
+  }
+
+  updateOrderParams(){
+    this.order_params.page = this.order_params.page + 1;
+    this.order_params.display_limit = this.order_params.page * 10;
+    this.getOrders();
+  }
+
+  navigateToOrderDetails(order){
+    console.log("Order ==>", order);
+    this.appservice.order = order;
+    // this.router.navigate([OrderDetailsComponent]);
+    let order_route = 'account/my-orders/' + order.order_info.txn_no;
+    this.router.navigate([order_route]);
+  }
+
+}
