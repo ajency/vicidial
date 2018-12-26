c4efec2c333d4ed0537455dd7452958e040ce074
sujitgawas19 <sujit@ajency.in>
2018-12-26 17:26:09 +0530

    reused old components for bag

---
+++ angular-app/src/app/bag/bag-view/bag-view.component.ts
@@ -0,0 +1,453 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppServiceService } from '../../service/app-service.service';
+import { ApiServiceService } from '../../service/api-service.service';
+import { Subscription } from 'rxjs/Subscription';
+
+import { LoginComponentComponent } from '../../shared-components/login/login-component/login-component.component';
+import { PromotionsListComponent } from '../../shared-components/promotions/promotions-list/promotions-list.component';
+import { AppliedCouponComponent } from '../../components/applied-coupon/applied-coupon.component';
+import { UpgradeCartComponent } from '../../components/upgrade-cart/upgrade-cart.component';
+import { BetterPromoAvailableComponent } from '../../components/better-promo-available/better-promo-available.component';
+import { BagSummaryComponent } from '../../shared-components/bag-summary/bag-summary/bag-summary.component';
+// import * as $ from 'jquery';
+declare var $: any;
+declare var add_to_cart_failed: any;
+declare var add_to_cart_failure_message: any;
+declare var add_to_cart_clicked: any;
+declare var add_to_cart_completed: any;
+
+declare var fbTrackInitiateCheckout : any;
+// declare var fbTrackuserRegistration : any;
+@Component({
+  selector: 'app-bag-view',
+  templateUrl: './bag-view.component.html',
+  styleUrls: ['./bag-view.component.css']
+})
+export class BagViewComponent implements OnInit {
+
+  enterCoupon = false;
+  cart : any = {};
+  sessionCheckInterval : any;
+  cartOpen = false;
+  reloadSubscription: Subscription;
+  loadSubscription: Subscription;
+  closeModalSubscription: Subscription;
+  openModalSubscription : Subscription;
+  cartItemOutOfStock : boolean = false;
+  fetchCartFailed : boolean = false;
+  isCartTypeFailure : boolean = false;
+  addToCartFailureMessage = '';
+  addToCartFailed : boolean = false;
+  promotions = [];
+  displayPromo : boolean = true;
+  constructor( private router: Router,
+               private appservice : AppServiceService,
+               private apiservice : ApiServiceService,
+               private zone : NgZone
+              ) { 
+    // this.createDummyPromotions();
+    this.reloadSubscription = this.appservice.listenToAddToCartEvent().subscribe(()=> { this.reloadCart() });
+    this.loadSubscription = this.appservice.listenToOpenCartEvent().subscribe(()=> { this.loadCart() });
+
+    this.closeModalSubscription = this.appservice.listenToCloseModal().subscribe(()=>{ this.updateOtpModal(false)});
+    this.openModalSubscription = this.appservice.listenToOpenModal().subscribe(()=>{ this.modalHandler()});
+
+  }
+
+  reloadCart(){
+    this.updateUrl();
+    console.log("listened to the add to cart trigger");
+    this.cartOpen = true;
+    this.fetchCartDataOnAddToCartSuccess();
+  }
+
+  loadCart(){
+    this.updateUrl();
+    console.log("listened to open cart trigger");
+    this.cartOpen = true;
+    this.getCartData();
+  }
+
+  ngOnDestroy() {
+  // unsubscribe to ensure no memory leaks
+    this.reloadSubscription.unsubscribe();
+    this.loadSubscription.unsubscribe();
+    this.closeModalSubscription.unsubscribe();
+    this.openModalSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit cart component");        
+      this.updateUrl();
+      this.cartOpen = true;
+      $('.ng-cart-loader').removeClass('cart-loader')
+      if(add_to_cart_clicked){
+        this.fetchCartDataOnAddToCartSuccess();
+        add_to_cart_clicked = false;
+      }
+      else{
+        this.getCartData();
+      }
+  }
+
+  ngAfterViewInit(){
+    if(window.location.href.endsWith('/user-verification'))
+      this.modalHandler();
+  }
+
+  updateUrl(){
+    // let url = window.location.href.split("#")[0] + '#bag';
+    // if(window.location.href.endsWith('#shipping-address') || window.location.href.endsWith('#shipping-summary'))
+    //   history.replaceState({cart : true}, 'cart', url);
+    // else if(!(window.location.href.endsWith('#bag') || window.location.href.endsWith('#bag/user-verification')))
+    //   history.pushState({cart : true}, 'cart', url);
+  }
+
+  fetchCartDataOnAddToCartSuccess(){    
+    this.appservice.showLoader();
+    if(sessionStorage.getItem('cart_data')){
+      this.cart = JSON.parse(sessionStorage.getItem('cart_data'));
+    }
+    else
+      this.cart = {};
+    
+    this.sessionCheckInterval = setInterval(()=>{
+      if(add_to_cart_completed){
+        this.fetchCartDataFromServer();
+        add_to_cart_completed = false;
+        clearInterval(this.sessionCheckInterval);
+      }
+    this.zone.run(() => {});
+    },100)
+    this.zone.run(() => {});
+  }
+
+  getCartData(){
+    this.appservice.showLoader()
+    if(sessionStorage.getItem('cart_data')){
+      this.cart = JSON.parse(sessionStorage.getItem('cart_data'));
+    }
+    this.fetchCartDataFromServer();
+  }
+
+  fetchCartDataFromServer(){
+    this.appservice.showLoader();
+    this.addToCartFailureMessage = '';
+    this.addToCartFailed = false;    
+    this.appservice.callFetchCartApi().then((response)=>{
+      console.log("promotions ==>", response.promotions);             
+      this.cart = this.formattedCartDataForUI(response);   
+      this.formatPromotions(response);          
+      this.checkCartItemOutOfStock();
+      this.appservice.removeLoader();
+      this.checkAppliedPromotionValidity();
+      this.updateLocalDataAndUI(this.cart, this.cart.cart_count);
+      console.log(add_to_cart_failed);
+      if(add_to_cart_failed){
+        console.log("add_to_cart_failed", add_to_cart_failure_message);
+        this.addToCartFailureMessage = add_to_cart_failure_message;
+        this.addToCartFailed = true;
+        add_to_cart_failed = false;
+        add_to_cart_failure_message = '';
+      }      
+      if(this.cart.cart_type == 'failure'){
+        this.editBag();
+        this.isCartTypeFailure = true;
+      }
+      this.fetchCartFailed = false;      
+      this.zone.run(() => {});
+    })
+    .catch((error)=>{
+      if(add_to_cart_failed){
+        console.log("add_to_cart_failed", add_to_cart_failure_message);
+        add_to_cart_failed = false;
+        add_to_cart_failure_message = '';
+      }
+      this.handleFetchCartFailure(error);
+      this.appservice.removeLoader();
+      this.zone.run(() => {});
+    })
+    this.zone.run(() => {});
+  }
+
+  formattedCartDataForUI(data){
+    data.items.forEach((item)=>{
+      if(item.attributes.price_mrp != item.attributes.price_final)
+        item.off_percentage = Math.round(((item.attributes.price_mrp - item.attributes.price_final) / (item.attributes.price_mrp )) * 100) + '% OFF';
+      item.href = '/' + item.product_slug +'/buy?size='+item.attributes.size;
+      item.attributes.images = Array.isArray(item.attributes.images) ? ['/img/placeholder.svg', '/img/placeholder.svg', '/img/placeholder.svg'] : Object.values(item.attributes.images);
+    })
+
+    data.items.sort((a,b)=>{ return a.timestamp - b.timestamp});
+    data.items.reverse();
+    return data;
+  }
+
+  modifyCart(item){
+    // console.log("inside modifyCart function ==>", item);
+    // let body;
+    // body = {
+    //   old_item : item.id,
+    //   new_item : item.related_items.size.find(size=> size.value == item.attributes.size).id,
+    //   quantity : item.quantity
+    // }
+    // console.log("Body ==>", body);
+    // let url = 'http://localhost:8000/rest/v1/anonymous/cart/update';
+    // this.apiservice.request(url, 'get', body ).then((response)=>{
+    //   console.log("response ==>", response);
+    //   item = response.item;
+    //   sessionStorage.setItem('cart_data', JSON.stringify(this.cart));
+    // })
+    // .catch((error)=>{
+    //   console.log("error ===>", error);
+    // })
+  }
+
+  deleteItem(item){
+    this.addToCartFailed = false;
+    this.appservice.showLoader();
+    let body = { variant_id : item.id };
+    let url = this.appservice.apiUrl + (this.appservice.isLoggedInUser() ? ("/api/rest/v1/user/cart/"+this.appservice.getCookie('cart_id')+"/delete?") : ("/rest/v1/anonymous/cart/delete?"));
+    let header = this.appservice.isLoggedInUser() ? { Authorization : 'Bearer '+this.appservice.getCookie('token') } : {};
+    url = url+$.param(body);
+    this.apiservice.request(url, 'get', body, header ).then((response)=>{
+      let index = this.cart.items.findIndex(i => i.id == item.id)
+      this.cart.items.splice(index,1);
+      this.cart.summary = response.summary;
+      this.cart.promo_applied = response.promo_applied;
+      this.cart.cart_count = response.cart_count;
+      this.displayPromo = true;
+      this.formatPromotions(response);
+      this.checkCartItemOutOfStock();
+      this.updateLocalDataAndUI(this.cart, this.cart.cart_count);
+      this.appservice.removeLoader()
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      if(error.status == 401){
+        this.appservice.userLogout();
+        this.fetchCartDataFromServer();
+        this.fetchCartFailed = false; 
+      }
+      else if((error.status == 400 || error.status == 403) && this.appservice.isLoggedInUser() ){
+        this.getNewCartId();
+        this.fetchCartFailed = false; 
+      }
+      this.appservice.removeLoader();
+    })
+  }
+
+  closeCart(){
+    let url = window.location.href.split("#")[0];
+    history.pushState({cart : false}, 'cart', url);
+    // this.reloadPage();
+    this.appservice.closeCart();
+  }
+
+  viewOrders(){
+      let url = window.location.href.split("#")[0] + '#/account/my-orders';
+      this.appservice.closeCart();
+      history.pushState({cart : false}, 'cart', url);
+      // window.location.href = url;
+      // this.reloadPage();
+  }
+
+  modalHandler(){
+    fbTrackInitiateCheckout(this.cart.summary.you_pay);
+    this.addToCartFailed = false;
+    if(this.appservice.isLoggedInUser()){
+      this.navigateToShippingDetailsPage();
+    }
+    else{
+      this.callCheckCartStatusApi();
+    }      
+  }
+
+  displayModal(){
+    let url = window.location.href +'/user-verification';
+    if(!window.location.href.endsWith('#bag/user-verification'))
+      history.pushState({cart : true}, 'cart', url);
+    $('#signin').modal('show');
+    $("#cd-cart").css("overflow", "hidden");
+    $('.modal-backdrop').appendTo('#cd-cart');
+    $('body').addClass('hide-scroll');
+  }
+
+
+  updateOtpModal(updateHistory : boolean = true){
+    $('#signin').modal('hide');
+    $("#cd-cart").css("overflow", "auto");
+    // this.mobileNumberEntered = false;
+    // this.otp = null;
+    // this.otpCode.otp1 =''; this.otpCode.otp2 = ''; this.otpCode.otp3 = ''; this.otpCode.otp4 = ''; this.otpCode.otp5 = ''; this.otpCode.otp6='';
+    // this.userValidation.otpVerificationErrorMsg = '';
+  }
+
+  navigateToShippingDetailsPage(){
+    console.log("navigateToShippingDetailsPage");
+    if(this.cart.cart_type == "cart"){
+      this.appservice.showLoader();
+      this.appservice.callGetAllAddressesApi(true).then((response)=>{
+        this.appservice.shippingAddresses = response.addresses;
+        $("#cd-cart").css("overflow", "auto");
+        $('.modal-backdrop').remove();
+        this.router.navigateByUrl('bag/shipping-address');
+        // this.router.navigate(['shipping-details']);
+        this.appservice.removeLoader();
+      })
+      .catch((error)=>{
+        console.log("error ===>", error);
+        this.appservice.removeLoader();
+        this.fetchCartDataFromServer();
+      })
+    }
+    else{
+      this.router.navigateByUrl('bag/shipping-summary');
+      // this.router.navigate(['shipping-summary']);
+      this.appservice.continueOrder = true;
+    }
+  }
+
+  checkCartItemOutOfStock(){
+    this.cartItemOutOfStock = false;
+    this.cart.items.forEach((item)=>{
+      if(!item.availability){
+        this.cartItemOutOfStock = true;
+        // break;
+      }
+    })
+  }
+
+  editBag(){
+    this.appservice.showLoader();
+    let url = this.appservice.apiUrl + '/api/rest/v1/user/cart/start-fresh';
+    let header = { Authorization : 'Bearer '+this.appservice.getCookie('token') };
+    this.apiservice.request(url, 'get', {} , header ).then((response)=>{
+      document.cookie='cart_id=' + response.cart_id + ";path=/";
+      this.fetchCartDataFromServer();
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      this.appservice.removeLoader();
+    })
+  }
+
+  reloadPage(){
+    window.location.reload();
+  }
+
+  getNewCartId(){
+    this.appservice.showLoader();
+    this.appservice.callMineApi().then((response)=>{
+      if(response.cart_id != this.appservice.getCookie('cart_id')){
+        document.cookie='cart_id=' + response.cart_id + ";path=/";
+        this.fetchCartDataFromServer();
+      }
+      else{
+        this.cart = {};
+        this.fetchCartFailed = true;
+      }
+    })
+    .catch((error)=>{
+      console.log("error : mine api===>", error);
+      this.appservice.removeLoader();
+    })
+  }
+
+  shopNow(){
+    window.location.href = '/shop'
+  }
+
+  updateLocalDataAndUI(cart : any = null, cart_count = 0){
+    console.log("isSessionStorageSupported ==>", this.isSessionStorageSupported());
+    if(this.isSessionStorageSupported()){
+      if(cart)
+          sessionStorage.setItem('cart_data', JSON.stringify(cart));
+      else
+        this.appservice.clearSessionStorage();
+    }
+    document.cookie = "cart_count=" + cart_count + ";path=/";
+    this.appservice.updateCartCountInUI();
+  }
+
+  handleFetchCartFailure(error){
+    if(error.status == 401){
+      this.appservice.userLogout();
+      this.fetchCartDataFromServer();
+      this.fetchCartFailed = false; 
+    }
+    else if((error.status == 400 || error.status == 403)){
+      if(this.appservice.isLoggedInUser()){
+        this.getNewCartId();
+        this.fetchCartFailed = false; 
+      }
+      else{
+        this.fetchCartDataFromServer();
+      }        
+    }
+    else if(error.status == 404){
+      this.cart = {
+        items : []
+      }
+      this.fetchCartFailed = false;
+      this.updateLocalDataAndUI();
+    }
+    else{
+      this.cart = {};
+      this.fetchCartFailed = true;
+      this.updateLocalDataAndUI();
+    }
+  }
+
+  callCheckCartStatusApi(){
+    this.appservice.showLoader();
+    let url = this.appservice.apiUrl + '/rest/v1/anonymous/cart/check-status';
+    this.apiservice.request(url, 'get', {} , {} ).then((response)=>{
+      this.appservice.removeLoader();
+      this.displayModal();
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      this.appservice.removeLoader();
+      this.fetchCartDataFromServer()
+    })
+  }
+
+  isSessionStorageSupported() {
+    try {
+      sessionStorage.setItem('test', 'test');
+      sessionStorage.removeItem('test');    
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  formatPromotions(response){
+    console.log(response);
+    let promos = Object.keys(response.promotions).map((k)=>{ return response.promotions[k] });
+    console.log("promos ==>",promos);
+    try{
+      promos.forEach((promo)=>{ 
+        promo.actual_discount = this.appservice.calculateDiscount(promo.discount_type, promo.discount_value, this.cart.summary.sale_price_total);
+        console.log(promo.actual_discount);
+      });
+    }
+    catch(e){
+      console.log("error ==>",e);
+    }
+    this.promotions = promos;
+  }
+
+  checkAppliedPromotionValidity(){
+    if(this.cart.promo_applied && this.cart.cart_type == 'order'){
+      let applied_promo = this.promotions.find((promotion)=>{ return this.cart.promo_applied == promotion.promotion_id});
+      console.log("checkAppliedPromotionValidity ==>",applied_promo)
+      if(!applied_promo){
+        this.editBag();
+      }
+    }
+  }
+  
+}
---
+++ angular-app/src/app/bag/shipping-summary/shipping-summary.component.ts
@@ -0,0 +1,173 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
+import { AppServiceService } from '../../service/app-service.service';
+import { ApiServiceService } from '../../service/api-service.service';
+import { BagSummaryComponent } from '../../shared-components/bag-summary/bag-summary/bag-summary.component';
+
+declare var $: any;
+declare var fbTrackAddPaymentInfo : any;
+
+@Component({
+  selector: 'app-shipping-summary',
+  templateUrl: './shipping-summary.component.html',
+  styleUrls: ['./shipping-summary.component.css']
+})
+export class ShippingSummaryComponent implements OnInit {
+
+  shippingDetails : any;
+  showUserInfoModal : boolean = false;
+  showCancelButton : boolean = false;
+  userName : any;
+  userEmail : any;
+  constructor(private router : Router,
+  			   		private appservice : AppServiceService,
+              private apiservice : ApiServiceService,
+              private route : ActivatedRoute
+  					) { }
+
+  ngOnInit() {
+    this.callOrderApi();
+    this.updateUrl();
+  }
+
+  navigateToPaymentPage(){
+    this.appservice.showLoader();
+    let url = this.appservice.apiUrl + '/api/rest/v1/user/order/' + this.shippingDetails.order_id + '/check-inventory'
+    let header = { Authorization : 'Bearer '+this.appservice.getCookie('token') };
+    this.apiservice.request(url, 'get', {} , header ).then((response)=>{
+      fbTrackAddPaymentInfo();
+      window.location.href = "/user/order/" + this.shippingDetails.order_id +"/payment/payu";      
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      this.router.navigateByUrl('/bag');
+      this.appservice.removeLoader();      
+    })      
+  }
+  
+  closeCart(){
+    let url = window.location.href.split("#")[0];
+    history.pushState({cart : false}, 'cart', url);
+    window.location.reload();
+  }
+
+  navigateBack(){
+    history.back();
+  }
+
+  updateUrl(){
+    // let url = window.location.href.split("#")[0] + '#shipping-summary';
+    // console.log("check url ==>", url);
+    // history.pushState({cart : true}, 'cart', url);      
+  }
+
+
+  callOrderApi(){
+    this.appservice.showLoader();
+    let url = this.getRequestUrl();
+    let header = { Authorization : 'Bearer '+this.appservice.getCookie('token') };
+    let body : any = {
+      _token : $('meta[name="csrf-token"]').attr('content')
+    };
+    if(this.appservice.selectedAddressId){
+      body.address_id = this.appservice.selectedAddressId;      
+    }
+    this.appservice.selectedAddressId = '';
+
+    this.apiservice.request(url, 'post', body , header ).then((response)=>{
+      this.shippingDetails = this.getProductUrl(response);
+      this.setUserName();
+      this.appservice.removeLoader();
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      this.router.navigateByUrl('/bag');
+      this.appservice.removeLoader();      
+    })  
+  }
+
+  getRequestUrl(){
+    if(this.appservice.continueOrder){
+      console.log("continue-order");
+      this.appservice.continueOrder = false;
+      return (this.appservice.apiUrl + '/api/rest/v1/user/cart/' + this.appservice.getCookie('cart_id') + '/continue-order');
+    }
+    else{
+      console.log("create-order");
+      return (this.appservice.apiUrl + '/api/rest/v1/user/cart/' + this.appservice.getCookie('cart_id') + '/create-order');
+    }    
+  }
+
+  getProductUrl(data){
+    data.items.forEach((item)=>{
+      item.href = '/' + item.product_slug +'/buy?size='+item.attributes.size;
+      item.attributes.images = Array.isArray(item.attributes.images) ? ['/img/placeholder.svg', '/img/placeholder.svg', '/img/placeholder.svg'] : Object.values(item.attributes.images);
+    })
+    return data;
+  }
+
+  saveUserInfo(){
+    this.appservice.showLoader();
+    let url = this.appservice.apiUrl + '/api/rest/v1/user/save-user-details';
+    let header = { Authorization : 'Bearer '+this.appservice.getCookie('token') };
+    let body : any = {
+      _token : $('meta[name="csrf-token"]').attr('content'),
+      name : this.shippingDetails.user_info.name,
+      email : this.shippingDetails.user_info.email
+    };
+
+    this.apiservice.request(url, 'post', body , header ).then((response)=>{
+      this.hideModal();
+      this.userEmail = body.email;
+      this.appservice.removeLoader();
+    })
+    .catch((error)=>{
+      console.log("error ===>", error);
+      this.appservice.removeLoader();
+    }) 
+  }
+
+  setUserName(){
+    if(!this.shippingDetails.user_info){
+      this.shippingDetails.user_info = {};
+      this.shippingDetails.user_info.name = this.shippingDetails.address.name;
+      this.shippingDetails.user_info.email = '';
+      setTimeout(()=>{
+        this.showModal();  
+      },100);
+      
+    }
+    else{
+      this.userEmail = this.shippingDetails.user_info.email;
+    }
+  }
+
+  editUserInfo(){
+    this.showCancelButton = true;
+    this.showModal();    
+  }
+
+  showModal(){
+    this.showUserInfoModal = true;
+    // var backdrop = $('.modal-backdrop');
+    $('#user-info').modal('show');
+    $("#cd-cart,.kss_shipping_summary").css("overflow", "hidden");
+    $('.modal-backdrop').appendTo('.scroll-container');
+    $('body').addClass('hide-scroll');
+  }
+
+  hideModal(){
+    this.showUserInfoModal = false;
+    $('#user-info').modal('hide');
+    $("#cd-cart,.kss_shipping_summary").css("overflow", "auto");
+    $('.modal-backdrop').remove();
+  }
+
+  editShippingAddress(){
+    this.appservice.directNavigationToShippingAddress = true;
+    this.appservice.editAddressFromShippingSummary = true;
+    this.appservice.addressToEdit = this.shippingDetails.address;
+    this.router.navigateByUrl('/shipping-details', { skipLocationChange: true });
+  }
+
+}
