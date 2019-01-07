ff61aa991c4287a2a30385eaae82f34da4827de5
Ajaj Rajguru <ajaj@ajency.in>
2019-01-07 13:32:01 +0530

    hotjar moved to footer

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -57,6 +57,18 @@
   gtag('config', "{{config('analytics.google_id')}}");
 </script>
 
+<!-- Hotjar Tracking Code -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:'{{config("analytics.hotjar.id")}}',hjsv:'{{config("analytics.hotjar.version")}}'};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 @yield('footjs')
 <script type="text/javascript">
 	window.lazySizesConfig = window.lazySizesConfig || {};
