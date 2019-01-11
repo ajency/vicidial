b669e92cd19164eaaad9ebe94f52c82ee4256350
unknown <rhugveda@ajency.in>
2019-01-11 18:49:25 +0530

    working on fetch method

--- app/StaticElement.php
+++ app/StaticElement.php
@@ -6,13 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class StaticElement extends Model
 {
-    public static function fetch(array $data = [], boolean $draft=true)
+    public static function fetch(array $data = [], bool $draft = true)
     {
-
-
-        
-
-        /*ifdata!empty
+       /*ifdata!empty
         $where = array();
         if isset $id
         $where['id'] = $id 
@@ -27,11 +23,11 @@ Facet::select('facet_value',DB::raw('count(id) as "count",facet_name'))->whereIn
         if($draft==true)
         {
             //select element_data_draft as elememt data and secquence_data_draft as sequence
-            $select = ['element_data_draft', 'sequence_data_draft'];
+            $select = ['id','type','element_data','element_data_draft', 'sequence','sequence_data_draft','published'];
         }
         else
         {
-            $select=['element_data','sequence_data'];
+            $select=['id','type','element_data','element_data_draft', 'sequence','sequence_data_draft','published'];
         }
 
         if(!empty($data))
@@ -41,35 +37,39 @@ Facet::select('facet_value',DB::raw('count(id) as "count",facet_name'))->whereIn
             if(isset($data['id']))
             {
                 //$where['id']=$id;
-                $where[] = ['id', '=', $id];
+                $where[] = ['id', '=', $data['id']];
             }
             if(isset($data['type']))
             {
-                $where[] = ['type', '=', $type];
+                $where[] = ['type', '=', $data['type']];
             }
-
-            $result=self::select($select)->where($where)->orderBy('sequence', 'ASC')->get();  
+           //select(DB::raw('count(*) as user_count, status'))
+            $staticElement=self::select($select)->where($where)->orderBy('sequence', 'ASC')->get();  
         }
         else
         {
-            $result=self::select($select)->orderBy('sequence', 'ASC')->get();
+            $staticElement=self::select($select)->orderBy('sequence', 'ASC')->get();
         }
-        
-        dd($result);
+       
+       
+        dd($staticElement);
 
 
         if(isset($data['id']))
         {
            $staticElementnew = $staticElement->first();
            $response=[
-                "id"=>$staticElementnew['id'],
-                "sequence"=>$staticElementnew['sequence'],
-                "element_data"=>$staticElementnew['element_data']
+                "id"=>$staticElement[0]['id'],
+                "sequence"=>$staticElement[0]['sequence'],
+                "element_data"=>$staticElement[0]['element_data']
             ];
  
+
         return(json_encode($response));
         }
        
+        
+
         foreach($staticElement as $k=>$v)
         {
                //dd($v['id']);
@@ -88,7 +88,7 @@ Facet::select('facet_value',DB::raw('count(id) as "count",facet_name'))->whereIn
                 );
          }//foreach
             
-    
+        // dd($response);
 
             
        return(json_encode($response));
