2dfcfe14b0d20b873d8e130a858a9aeb24725599
Ajaj Rajguru <ajaj@ajency.in>
2019-01-25 11:52:39 +0530

    New year sale category replaced with Freedom category

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -263,7 +263,7 @@
   <div class="container">
     <div class="cat-container">
       <div class="cat-item cat-13">
-        <a href="/shop">
+        <!-- <a href="/shop">
           <img class="d-block w-100 img-fluid lazyload blur-up"
               src="{{CDN::asset('/img/home-category/category_grid13_10px.jpg') }}"
               data-srcset="{{CDN::asset('/img/home-category/category_grid13_large.jpg') }} 740w,
@@ -272,6 +272,16 @@
               data-sizes='(min-width: 1200px) 570px, (min-width: 768px) and (max-width: 991px) 46vw, (max-width: 992px) 92vw,  31vw'
               alt="New Year Super Sale - Flat 50% OFF"
               title="New Year Super Sale - Flat 50% OFF" />
+        </a> -->
+        <a href="/shop">
+          <img class="d-block w-100 img-fluid lazyload blur-up"
+              src="{{CDN::asset('/img/home-category/category_freedom_10px.jpg') }}"
+              data-srcset="{{CDN::asset('/img/home-category/category_freedom_large.jpg') }} 740w,
+                            {{CDN::asset('/img/home-category/category_freedom_medium.jpg') }} 370w,
+                            {{CDN::asset('/img/home-category/category_freedom_small.jpg') }} 248w"
+              data-sizes='(min-width: 1200px) 570px, (min-width: 768px) and (max-width: 991px) 46vw, (max-width: 992px) 92vw,  31vw'
+              alt="Get upto 69% off This Republic Day"
+              title="Get upto 69% off This Republic Day" />
         </a>
       </div>
       <div class="cat-item cat-14">
