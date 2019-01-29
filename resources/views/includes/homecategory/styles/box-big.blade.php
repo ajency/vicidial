353a08b4a340bf5061c769673764f8376ece590f
sujitgawas19 <sujit@ajency.in>
2019-01-29 15:58:31 +0530

    blade changes

--- resources/views/includes/homecategory/styles/box-big.blade.php
+++ resources/views/includes/homecategory/styles/box-big.blade.php
@@ -1,6 +1,6 @@
 <div class="cat-item cat-{{$category['sequence']}} position-relative kss-extension" static_element-id="{{$category['sequence']}}" static_element-display_type="Styles In Focus" static_element-type="{{$category['type']}}">
   <a href="{{$category['element_data']['image']['href']}}">
-    <div class="custom-text text-uppercase">{!!$category['element_data']['text']['text1']!!}</div>
+    <div>{!!$category['element_data']['text']['text1']!!}</div>
     @if (isset($category['images']['original']))
     <img class="d-block w-100 img-fluid lazyload blur-up" src="{{$category['images']['default']['load']}}" 
     data-srcset="{{$category['images']['original']}}" alt="{{$category['element_data']['image']['img_alt']}}" title="{{$category['element_data']['image']['title']}}" />
@@ -14,6 +14,6 @@
         alt="{{$category['element_data']['image']['img_alt']}}"
         title="{{$category['element_data']['image']['title']}}" />
     @endif
-    <div class="custom-text text-uppercase">{!!$category['element_data']['text']['text2']!!}</div>
+    <div>{!!$category['element_data']['text']['text2']!!}</div>
   </a>
 </div>
--- resources/views/includes/stories/styles/box-medium.blade.php
+++ resources/views/includes/stories/styles/box-medium.blade.php
@@ -1,6 +1,6 @@
 <div class="story-{{$story['sequence']}} position-relative kss-extension" static_element-id="{{$story['sequence']}}" static_element-display_type="Shop By Stories" static_element-type="{{$story['type']}}">
     <a href="{{$story['element_data']['image']['href']}}" class="position-relative trend-box d-block link-card" style="order:0;">
-        <div class="custom-text text-uppercase">{!!$story['element_data']['text']['text1']!!}</div>
+        <div>{!!$story['element_data']['text']['text1']!!}</div>
         @if (isset($story['images']['original']))
         <img class="d-block w-100 img-fluid lazyload blur-up pb-1" src="{{$story['images']['default']['load']}}" 
         data-srcset="{{$story['images']['original']}}" alt="{{$story['element_data']['image']['img_alt']}}" title="{{$story['element_data']['image']['title']}}" />
@@ -13,7 +13,7 @@
             data-sizes='(min-width: 1200px) 376px, (min-width: 768px) and (max-width: 991px) 46vw, (max-width: 992px) 90vw,  30vw'
             alt="{{$story['element_data']['image']['img_alt']}}"
             title="{{$story['element_data']['image']['title']}}"/>
-        <div class="custom-text text-uppercase">{!!$story['element_data']['text']['text2']!!}</div>
+        <div>{!!$story['element_data']['text']['text2']!!}</div>
         @endif
     </a>
 </div>
