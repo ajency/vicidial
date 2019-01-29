ae1b3fcf5f43fa491e89edf88fa9e357a9d694ce
Viraj Verlekar <viraj@ajency.in>
2019-01-29 14:57:49 +0530

    Escape text as html

--- resources/views/includes/stories/styles/landscape.blade.php
+++ resources/views/includes/stories/styles/landscape.blade.php
@@ -1,6 +1,6 @@
 <div class="story-{{$story['sequence']}} position-relative kss-extension" static_element-id="{{$story['sequence']}}" static_element-display_type="Shop By Stories" static_element-type="{{$story['type']}}">
     <a href="{{$story['element_data']['image']['href']}}" class="position-relative trend-box d-block link-card" style="order:0;">
-        <p class="custom-text text-uppercase">{{$story['element_data']['text']['text1']}}</p>
+        <p class="custom-text text-uppercase">{!!$story['element_data']['text']['text1']!!}</p>
         @if (isset($story['images']['original']))
         <img class="d-block w-100 img-fluid lazyload blur-up pb-1" src="{{$story['images']['default']['load']}}" 
         data-srcset="{{$story['images']['original']}}" alt="{{$story['element_data']['image']['img_alt']}}" title="{{$story['element_data']['image']['title']}}" />
@@ -13,7 +13,7 @@
             data-sizes='(min-width: 1200px) 780px, (max-width: 992px) 90vw, 63vw'
             alt="{{$story['element_data']['image']['img_alt']}}"
             title="{{$story['element_data']['image']['title']}}"/>
-        <p class="custom-text text-uppercase">{{$story['element_data']['text']['text2']}}</p>
+        <p class="custom-text text-uppercase">{!!$story['element_data']['text']['text2']!!}</p>
          @endif
     </a>
 </div>
