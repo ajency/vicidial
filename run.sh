81741225f83d8a2456d112745728cdbee18ab4cf
Shashank Shetye Saudagar <shashank@ajency.in>
2019-01-31 22:19:12 +0530

    fix sequence

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ service supervisor start
 supervisorctl reread 
 supervisorctl update 
 supervisorctl start all 
-php /var/www/html/artisan migrate --force
 php /var/www/html/artisan cdn:push
 php /var/www/html/artisan queue:restart
 chown -R www-data /var/www/html/
+php /var/www/html/artisan migrate --force
 exec "$@"
