eae03735005b766f143f8ea36a656fa7e0d86a80
unknown <rhugveda@ajency.in>
2019-02-04 12:05:52 +0530

    added migrations

---
+++ database/migrations/2019_02_04_063349_change_unique_constraints_of_static_elements.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUniqueConstraintsOfStaticElelments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('static_elements', function (Blueprint $table) {
+            $table->dropUnique('static_elements_sequence_type_published_unique');
+            $table->dropUnique('static_elements_sequence_type_draft_unique');
+            $table->unique(array('sequence', 'type','published','page_slug'));
+            $table->unique(array('sequence','type','draft','page_slug'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
