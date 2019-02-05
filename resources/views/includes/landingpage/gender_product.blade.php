cf9133cc298e16d438927b295cc7b05d1370b6cf
sujitgawas19 <sujit@ajency.in>
2019-02-05 12:00:14 +0530

    img attribute changed

--- resources/views/includes/landingpage/gender_product.blade.php
+++ resources/views/includes/landingpage/gender_product.blade.php
@@ -1,7 +1,7 @@
 <div class="product-group__col">							
 	<a href="/indigo-solid-shirt-449-mid-wash/buy">
 		<img src="{{$product['images']['load']}}" 
-		data-srcset="{{$product['images']['1x']}} 270w, 
+		srcset="{{$product['images']['1x']}} 270w, 
 					{{$product['images']['2x']}} 540w, 
 					{{$product['images']['3x']}} 810w" 
 		data-sizes="(min-width: 1200px) 182px,(min-width: 992px) 13vw,(min-width: 768px) 33vw, 53vw" 
