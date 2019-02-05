6bd154ac622c692d2fb96ed7e371c906a85768d3
Ajaj Rajguru <ajaj@ajency.in>
2019-02-05 15:12:54 +0530

    landing page removed unused variables

--- resources/views/includes/landingpage/gender.blade.php
+++ resources/views/includes/landingpage/gender.blade.php
@@ -4,53 +4,13 @@
   if ($gendername == 'boys') {
   	$title = 'Boys';
   	$years = '2 - 7 Years';
-  	$box_1_alt= 'Shirts';
-  	$box_1_title= 'Shirts';
-  	$box_2_alt= 'T-Shirts';
-  	$box_2_title= 'T-Shirts';
-  	$box_3_alt= 'Ethnic';
-  	$box_3_title= 'Ethnic';
-  	$box_4_alt= 'Bottoms';
-  	$box_4_title= 'Bottoms';
-  	$box_5_alt= 'Shorts';
-  	$box_5_title= 'Shorts';
-  	$box_6_alt= 'Shoes';
-  	$box_6_title= 'Shoes';
   } else if ($gendername == 'girls') {
   	$title = 'Girls';
   	$years = '2 - 7 Years';
-  	$box_1_alt= 'Dresses';
-  	$box_1_title= 'Dresses';
-  	$box_2_alt= 'T-Shirts';
-  	$box_2_title= 'T-Shirts';
-  	$box_3_alt= 'Woven Tops';
-  	$box_3_title= 'Woven Tops';
-  	$box_4_alt= 'Ethnic';
-  	$box_4_title= 'Ethnic';
-  	$box_5_alt= 'Denim';
-  	$box_5_title= 'Denim';
-  	$box_6_alt= 'Bottoms';
-  	$box_6_title= 'Bottoms';
-  	$box_7_alt= 'Shorts';
-  	$box_7_title= 'Shorts';
-  	$box_8_alt= 'Shoes';
-  	$box_8_title= 'Shoes';
   }
   else{
 	$title = 'Infants';
 	$years = '0 - 2 Years';
-	$box_1_alt= 'Dresses';
-  	$box_1_title= 'Dresses';
-  	$box_2_alt= 'T-Shirts';
-  	$box_2_title= 'T-Shirts';
-  	$box_3_alt= 'Bottoms';
-  	$box_3_title= 'Bottoms';
-  	$box_4_alt= 'Utility';
-  	$box_4_title= 'Utility';
-  	$box_5_alt= 'Shoes';
-  	$box_5_title= 'Shoes';
-  	$box_6_alt= '';
-  	$box_6_title= '';
   }
 @endphp
 
@@ -99,19 +59,5 @@
 	@endforeach	
 	@endif	
 </div>
-
-@stop
-
-
-@section('footjs')
-
-<script type="text/javascript">
-	// $(function(){
-	// 	$('.sublanding-container > .row').each(function(){
-	// 		var title_years = $(this).find('.m-detach').detach();
-	// 		$(this).find('.img-hero').append(title_years);
-	// 	})
-	// });
-</script>
 
 @stop
--- resources/views/includes/landingpage/gender_product.blade.php
+++ resources/views/includes/landingpage/gender_product.blade.php
@@ -1,11 +1,11 @@
 <div class="product-group__col">							
-	<a href="/indigo-solid-shirt-449-mid-wash/buy">
+	<a href="/indigo-solid-shirt-449-mid-wash/buy" class="w-100">
 		<img src="{{$product['images']['load']}}" 
 		data-srcset="{{$product['images']['1x']}} 270w, 
 					{{$product['images']['2x']}} 540w, 
 					{{$product['images']['3x']}} 810w" 
 		data-sizes="(min-width: 1200px) 182px,(min-width: 992px) 13vw,(min-width: 768px) 33vw, 53vw" 
-		class="card-img-top blur-up lazyload" 
+		class="card-img-top blur-up lazyload d-block w-100" 
 		title="{{$product['title']}}" 
 		alt="{{$product['title']}}">
 	</a>
