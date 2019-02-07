b6a38949c17cb0628caf4d1f32b7bc449b19f73e
Viraj Verlekar <viraj@ajency.in>
2019-02-07 11:23:32 +0530

    Config changes

--- resources/views/includes/landingpage/gender_banner.blade.php
+++ resources/views/includes/landingpage/gender_banner.blade.php
@@ -1,20 +1,20 @@
 <picture>
    <source media="(orientation: landscape)"   		
-	  data-srcset="{{$banner['images']['landscape']['3x']}} 1536w,
-	              {{$banner['images']['landscape']['2x']}} 1024w,
-	              {{$banner['images']['landscape']['1x']}} 512w"	                          
+	  data-srcset="{{$landing['images']['landscape']['3x']}} 1536w,
+	              {{$landing['images']['landscape']['2x']}} 1024w,
+	              {{$landing['images']['landscape']['1x']}} 512w"	                          
 			        sizes="57vw">
 
    <source media="(orientation: portrait)"
-		data-srcset="{{$banner['images']['portrait']['3x']}} 1200w,
-				    {{$banner['images']['portrait']['2x']}} 700w,
-		      		{{$banner['images']['portrait']['1x']}} 400w"
+		data-srcset="{{$landing['images']['portrait']['3x']}} 1200w,
+				    {{$landing['images']['portrait']['2x']}} 700w,
+		      		{{$landing['images']['portrait']['1x']}} 400w"
 				sizes="100vw">
 
    <img   	   
-		src="{{$banner['images']['landscape']['load']}}"
+		src="{{$landing['images']['landscape']['load']}}"
 		data-sizes="100vw"
 		class="img-fluid lazyload blur-up w-100"
-		alt="{{$banner['element_data']['image']['img_alt']}}"
-		title="{{$banner['element_data']['image']['title']}}">
+		alt="{{$landing['element_data']['image']['img_alt']}}"
+		title="{{$landing['element_data']['image']['title']}}">
 </picture>
