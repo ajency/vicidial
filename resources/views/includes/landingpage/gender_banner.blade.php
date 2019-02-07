a922b3f3be667ab49cd16233066f9128276135d6
Ajaj Rajguru <ajaj@ajency.in>
2019-02-07 16:04:24 +0530

    landing page banner media query updated

--- resources/views/includes/landingpage/gender_banner.blade.php
+++ resources/views/includes/landingpage/gender_banner.blade.php
@@ -3,13 +3,13 @@
 	  data-srcset="{{$landing['images']['landscape']['3x']}} 1536w,
 	              {{$landing['images']['landscape']['2x']}} 1024w,
 	              {{$landing['images']['landscape']['1x']}} 512w"	                          
-			        sizes="57vw">
+			        media="(min-width: 1200) 48vw, (min-width: 992px) 57vw">
 
    <source media="(orientation: portrait)"
 		data-srcset="{{$landing['images']['portrait']['3x']}} 1200w,
 				    {{$landing['images']['portrait']['2x']}} 700w,
 		      		{{$landing['images']['portrait']['1x']}} 400w"
-				sizes="100vw">
+				media="(min-width: 768px) 57vw, 100vw">
 
    <img   	   
 		src="{{$landing['images']['landscape']['load']}}"
