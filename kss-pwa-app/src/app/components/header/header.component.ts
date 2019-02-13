d50f36cce454261a578e783641d7d3eb1dd87015
sujitgawas19 <sujit@ajency.in>
2019-02-13 16:33:54 +0530

    added js for menu open, close and toggle

--- kss-pwa-app/src/app/components/header/header.component.ts
+++ kss-pwa-app/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,  Input, OnChanges } from '@angular/core';
-declare var $: any;
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +13,13 @@ export class HeaderComponent implements OnInit, OnChanges {
   constructor(){ }
 
   ngOnInit(){
+    $('.megamenu--left .nav-item').click(function(){
+      var menuTab = $(this);
+      menuTab.addClass('active').siblings().removeClass('active');
+      var mobMenuName = menuTab.data('target');
+      $('.megamenu-wrapper').addClass('d-none');
+      $('.megamenu-wrapper[data-menu="'+mobMenuName+'"]').removeClass('d-none');
+    });
   }
 
   ngOnChanges(){
@@ -25,5 +32,12 @@ export class HeaderComponent implements OnInit, OnChanges {
 		$('.megamenu--right li .megamenu-wrapper').addClass('d-none');
 		$('.megamenu--right li:first-child .megamenu-wrapper').removeClass('d-none');
 		$('body').addClass('overflow-h');
+  }
+
+  closeMenu(){
+    $('.megamenu').removeClass('active');
+    $('.megamenu--left .nav-item').removeClass('active');
+    $('.megamenu-wrapper').addClass('d-none');
+    $('body').removeClass('overflow-h');
   }
 }
