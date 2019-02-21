02c70949f4e6963a119c6130d44b8b7808ce5764
sujitgawas19 <sujit@ajency.in>
2019-02-21 16:31:28 +0530

    integrated category compoenent

--- kss-pwa-app/src/app/home/components/categories/categories.component.ts
+++ kss-pwa-app/src/app/home/components/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, AfterViewInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: '[app-categories]',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
+	@Input() categories : any;
   constructor() { }
 
   ngOnInit() {
