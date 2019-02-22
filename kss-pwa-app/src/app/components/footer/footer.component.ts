3582bed9f536d570837ba5362a6a536ada750dd4
Ajaj Rajguru <ajaj@ajency.in>
2019-02-22 15:08:09 +0530

    footer updated

--- kss-pwa-app/src/app/components/footer/footer.component.ts
+++ kss-pwa-app/src/app/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +12,11 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$('.footer-more').click(function(e){
+  		e.preventDefault();
+  		$(this).toggleClass('collapsed');
+  		$('.hidden-footer-section').toggleClass('show');
+  	});
   }
 
 }
