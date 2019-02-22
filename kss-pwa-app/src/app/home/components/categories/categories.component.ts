dd70df10197a0f63b4bbc2870822815f6c31dd41
Ajaj Rajguru <ajaj@ajency.in>
2019-02-22 12:44:55 +0530

    category mobile js added + query installed

--- kss-pwa-app/src/app/home/components/categories/categories.component.ts
+++ kss-pwa-app/src/app/home/components/categories/categories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, OnChanges, AfterViewInit, Output, EventEmitter } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: '[app-categories]',
   templateUrl: './categories.component.html',
@@ -9,6 +11,8 @@ export class CategoriesComponent implements OnInit {
 
 	@Input() categories : any;
 	catFlag : boolean = false;
+	isMobile : any;
+
   constructor() { }
 
   ngOnInit() {
@@ -16,30 +20,42 @@ export class CategoriesComponent implements OnInit {
 
   ngAfterViewInit(){
   	this.categoryHeight();
+  	this.isMobile = this.isMobileScreen();
   }
 
+	isMobileScreen(){
+	  if ($(window).width() < 768)
+	    return true
+	  else
+	    return false
+	}
+
+
   categoryHeight(){
-    var catHeight = document.getElementById('cat-1').clientHeight;
-    var box_height = (catHeight * 5) + 80;
-    document.getElementById('home-category').style.height = box_height + 'px';
-	document.getElementById('home-category').classList.add('overflow-h');
-    this.catFlag = true;
+  	if(this.isMobile){
+	    var catHeight = document.getElementById('cat-1').clientHeight;
+	    var box_height = (catHeight * 5) + 80;
+	    document.getElementById('home-category').style.height = box_height + 'px';
+		document.getElementById('home-category').classList.add('overflow-h');
+	    this.catFlag = true;
+	}
   }
 
   showMoreCat(){
-  	console.log('show more');
-    if(this.catFlag){
-      document.getElementById('home-category').style.height = 'auto';
-      document.getElementById('home-category').classList.remove('overflow-h');
-      document.getElementById('show-more').innerHTML = 'Show Less';
-      document.getElementById('show-more').classList.add('icon-turn');
-      this.catFlag = false;
-    }
-    else{
-      this.categoryHeight();
-      document.getElementById('show-more').innerHTML = 'Show More';
-      document.getElementById('show-more').classList.remove('icon-turn');
-    }
+  	if(this.isMobile){
+	    if(this.catFlag){
+	      document.getElementById('home-category').style.height = 'auto';
+	      document.getElementById('home-category').classList.remove('overflow-h');
+	      document.getElementById('show-more').innerHTML = 'Show Less';
+	      document.getElementById('show-more').classList.add('icon-turn');
+	      this.catFlag = false;
+	    }
+	    else{
+	      this.categoryHeight();
+	      document.getElementById('show-more').innerHTML = 'Show More';
+	      document.getElementById('show-more').classList.remove('icon-turn');
+	    }
+	}
   }
 
 
