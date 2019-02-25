a9aab0db0bb246521c471ee48c2a7c8ac4cb0a6c
jeromie <jeromie@ajency.in>
2019-02-25 12:44:44 +0530

    optimize and tag manager ids added

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -65,7 +65,7 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', "{{config('analytics.google_id')}}");
+  gtag('config', "{{config('analytics.google_id')}}", { 'optimize_id': "{{config('analytics.google_optimize')}}"});
 </script>
 
 <!-- Google pixel tracking -->
@@ -96,6 +96,19 @@
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{{config('analytics.google_tag_manager')}}');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{config('analytics.google_tag_manager')}}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 @yield('footjs')
 <script type="text/javascript">
