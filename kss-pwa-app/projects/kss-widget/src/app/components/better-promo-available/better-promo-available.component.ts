ecfda9cc944038c38270293de36869a0bf692163
sujitgawas19 <sujit@ajency.in>
2019-02-25 16:29:26 +0530

    created bag and account as sub-application in angular 7

---
+++ kss-pwa-app/projects/kss-widget/src/app/components/better-promo-available/better-promo-available.component.ts
@@ -0,0 +1,36 @@
+import { Component,  OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { AppServiceService } from '../../service/app-service.service';
+
+@Component({
+  selector: 'app-better-promo-available',
+  templateUrl: './better-promo-available.component.html',
+  styleUrls: ['./better-promo-available.component.css']
+})
+export class BetterPromoAvailableComponent implements OnInit, OnChanges {
+	@Input() promoApplied : any;
+	@Input() promotions : any;
+	@Input() orderTotal : any;
+
+	promo : any;
+  constructor(private appservice : AppServiceService) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(){
+  	console.log("ngOnChanges better-promo-available component ==>", this.promoApplied, this.promotions);
+  	let filterd_obj = this.appservice.filterArray(this.promotions, this.orderTotal);
+  	let applicable = filterd_obj.applicable;
+  	console.log(applicable);
+  	if(applicable.length){
+  		let sorted_array = this.appservice.sortByDiscount(applicable);
+  		// sorted_array.reverse();
+	  	console.log("sorted_array ==>", sorted_array);
+	  	let applied_promo = this.promotions.find((promotion)=>{ return this.promoApplied == promotion.promotion_id});
+	  	if(applied_promo && (applied_promo.actual_discount < sorted_array[0].actual_discount)){
+	  		this.promo = sorted_array[0];
+	  	}
+  	}  	
+  }
+
+}
