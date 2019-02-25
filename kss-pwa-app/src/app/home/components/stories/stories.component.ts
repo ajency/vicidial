50308c6f92a01b24ff098063bb7fe659a6367dae
Ajaj Rajguru <ajaj@ajency.in>
2019-02-25 12:50:07 +0530

    story mobile show more added

--- kss-pwa-app/src/app/home/components/stories/stories.component.ts
+++ kss-pwa-app/src/app/home/components/stories/stories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, OnChanges, AfterViewInit, Output, EventEmitter } from '@angular/core';
 
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
@@ -8,11 +10,28 @@ import { Component, OnInit, Input, OnChanges, AfterViewInit, Output, EventEmitte
 export class StoriesComponent implements OnInit, OnChanges, AfterViewInit {
 
 	@Input() stories : any;
+    catFlag : boolean = false;  
+    isSmallScreen : any;
+    toggleButton = "SHOW MORE";
   @Output() storiesLoaded = new EventEmitter();
-  constructor() {
+  constructor(private breakpointObserver : BreakpointObserver) {
+    this.isSmallScreen = this.breakpointObserver.isMatched('(max-width: 768px)');
+    // console.log("isSmallScreen==>", this.isSmallScreen);
   }
 
   ngOnInit() {
+    this.breakpointObserver.observe([
+    '(max-width: 768px)'
+      ]).subscribe(result => {
+        console.log(result);
+        if (result.matches) {
+          console.log("mobile view");
+          this.isSmallScreen = true;
+        }else {
+          console.log("desktop view");
+          this.isSmallScreen = false;
+        }
+    });
   }
 
   ngOnChanges(){
@@ -22,10 +41,38 @@ export class StoriesComponent implements OnInit, OnChanges, AfterViewInit {
   ngAfterViewInit(){
     console.log("ngAfterViewInit stories component");
     this.storiesLoaded.emit(true);
+    if(this.isSmallScreen)
+      this.storyHeight();
   }
 
   createDataSrcSet(a,b,c,d){
     return a+ " " +b +", " +c +" "+d;
+  }
+
+  storyHeight(){
+    var catHeight = document.getElementById('story-1').clientHeight;
+    var box_height = (catHeight * 7);
+    document.getElementById('home-story').style.height = box_height + 'px';
+     document.getElementById('home-story').classList.add('overflow-h');
+    this.catFlag = true;
+  }
+
+  showMoreStory(){
+    console.log('show more');
+    if(this.catFlag){
+      document.getElementById('home-story').style.height = 'auto';
+      document.getElementById('home-story').classList.remove('overflow-h');
+      // document.getElementById('show-more').innerHTML = 'Show Less';
+      this.toggleButton = "SHOW LESS";
+      document.getElementById('show-more-story').classList.add('icon-turn');
+      this.catFlag = false;
+    }
+    else{
+      this.storyHeight();
+      // document.getElementById('show-more').innerHTML = 'Show More';
+      this.toggleButton = "SHOW MORE";
+      document.getElementById('show-more-story').classList.remove('icon-turn');
+    }
   }
 
 }
