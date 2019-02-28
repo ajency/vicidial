5bbe19508d2e1fb35b1e24580dc568d2a783bd71
Ajaj Rajguru <ajaj@ajency.in>
2019-02-28 13:26:08 +0530

    Related links + stories link on single product page updated

--- resources/views/includes/related-searches.blade.php
+++ resources/views/includes/related-searches.blade.php
@@ -1,30 +1,44 @@
-<div class="container mt-4">
-	<ul class="related-search d-flex align-items-center p-0">
+<div class="container mt-4 mt-md-5 py-4">
+	<!-- For Additional/Related links -->
+	<ul class="related-search d-flex align-items-center py-3 py-md-0 pl-0 mb-0">
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				Boys Bottom
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				Boys Bottom
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				Boys Bottom
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
-	</ul>
-	<ul class="related-search d-flex align-items-center p-0 stories-links mt-2">
+	</ul>	
+	<!-- Stories link -->
+	<ul class="related-search d-flex align-items-center py-3 py-md-0 pl-0 stories-links mt-md-5">
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				"How I wonder what you are"
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				"First Love"
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
 		<li class="related-search__links list-unstyled">
-			<a href="#">Boys Bottom</a>
-			<i class="fas fa-chevron-right d-md-none"></i>
+			<a href="#">
+				"Baby Mornings"
+				<i class="fas fa-chevron-right d-md-none"></i>
+			</a>
 		</li>
 	</ul>
 </div>
