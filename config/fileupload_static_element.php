e6a37add507fd92e7e8e709a39ebf1b161d28198
sujitgawas19 <sujit@ajency.in>
2019-03-01 14:11:34 +0530

    added config for new static elements

--- config/fileupload_static_element.php
+++ config/fileupload_static_element.php
@@ -217,4 +217,126 @@ return [
     ],
     'trending_presets'=> [],
     'trending_upload'=> [],
+
+    'stories_presets'=> [
+        "default" => [
+            "1x"=>"360X360",
+            "2x"=>"720X720",
+            "3x"=>"1080X1080",
+            "load"=>"10X13" 
+        ],
+    ],
+    'stories_upload'=> [
+        "default" => [
+            "size" => 110000,
+            "size_gif" => 500000,
+            "height" => 1080,
+            "width" => 1080
+        ],
+    ],
+
+    'categories_presets'=> [
+        "default" => [
+            "1x"=>"300X300",
+            "2x"=>"600X600",
+            "3x"=>"900X900",
+            "load"=>"10X13" 
+        ],
+    ],
+    'categories_upload'=> [
+        "default" => [
+            "size" => 135000,
+            "size_gif" => 500000,
+            "height" => 900,
+            "width" => 900
+        ],
+    ],
+
+    'brand_presets'=> [
+        "default" => [
+            "1x"=>"170X170",
+            "2x"=>"340X340",
+            "3x"=>"510X510",
+            "load"=>"10X13" 
+        ],
+    ],
+    'brand_upload'=> [
+        "default" => [
+            "size" => 40000,
+            "size_gif" => 500000,
+            "height" => 510,
+            "width" => 510
+        ],
+    ],
+
+    'offer_presets'=> [
+        "default" => [
+            "1x"=>"350X350",
+            "2x"=>"700X700",
+            "3x"=>"1050X1050",
+            "load"=>"10X13" 
+        ],
+    ],
+    'offer_upload'=> [
+        "default" => [
+            "size" => 130000,
+            "size_gif" => 500000,
+            "height" => 1050,
+            "width" => 1050
+        ],
+    ],
+
+
+    'week_theme_presets'=> [
+        "landscape" => [
+            "1x"=>"600X300",
+            "2x"=>"1200X600",
+            "3x"=>"1800X900",
+            "load"=>"20X7"
+        ],
+        "portrait" => [
+            "1x"=>"400X311",
+            "2x"=>"700X544",
+            "3x"=>"1200X933" 
+        ],
+    ],
+    'week_theme_upload'=> [
+        "landscape" => [
+            "size" => 140000,
+            "height" => 900,
+            "width" => 1800
+        ],
+        "portrait" => [
+            "size" => 120000,
+            "height" => 933,
+            "width" => 1200
+        ],
+    ],
+
+    'month_theme_presets'=> [
+        "landscape" => [
+            "1x"=>"600X300",
+            "2x"=>"1200X600",
+            "3x"=>"1800X900",
+            "load"=>"20X7"
+        ],
+        "portrait" => [
+            "1x"=>"400X311",
+            "2x"=>"700X544",
+            "3x"=>"1200X933" 
+        ],
+    ],
+    'month_theme_upload'=> [
+        "landscape" => [
+            "size" => 140000,
+            "height" => 900,
+            "width" => 1800
+        ],
+        "portrait" => [
+            "size" => 120000,
+            "height" => 933,
+            "width" => 1200
+        ],
+    ]
+
 ];
