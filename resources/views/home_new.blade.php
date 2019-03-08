91a6fc830d6f59a22c7255e2f84ac0e277f260e5
jeromie <jeromie@ajency.in>
2019-03-08 12:29:53 +0530

    ui fixes

--- resources/views/home_new.blade.php
+++ resources/views/home_new.blade.php
@@ -4,12 +4,12 @@
   <meta charset="utf-8">
   <title>Kidsuperstore</title>
   <base href="/">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
   <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0 shrink-to-fit=no'/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link  rel="icon" type="image/x-icon" href="{{CDN::asset('/img/fav/fav-64.png') }}" />
   <link rel="manifest" href="manifest.json">
   <meta name="theme-color" content="#707279"/>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
   <link rel="stylesheet" href="views/kss-pwa/styles.css">
   {!! SEOMeta::generate() !!}
   {!! OpenGraph::generate() !!}
