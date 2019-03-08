1addc2c2c109d3f7c61034ba04b6f423effd304b
jeromie <jeromie@ajency.in>
2019-03-08 15:08:42 +0530

    minor fixes

--- resources/views/home_new.blade.php
+++ resources/views/home_new.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Kidsuperstore</title>
   <base href="/">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
   <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0 shrink-to-fit=no'/>
