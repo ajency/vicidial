cd39da3ae56f69b05f8489901880e33cb3f02621
sujitgawas19 <sujit@ajency.in>
2019-03-18 13:57:51 +0530

    configured php route for product page

--- kss-pwa-app/src/app/product-page/product-page/product-page.component.ts
+++ kss-pwa-app/src/app/product-page/product-page/product-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ApiServiceService } from '../../service/api-service.service';
 import { AppServiceService } from '../../service/app-service.service';
@@ -11,16 +11,26 @@ export class ProductPageComponent implements OnInit {
 
 	product : any;
   showLoader : boolean = false;
+  menuObject : any
   constructor(private route: ActivatedRoute,
   			  private apiService: ApiServiceService,
               private appservice : AppServiceService) { }
 
   ngOnInit() {
+    let url = isDevMode() ? "https://demo8558685.mockable.io/get-menu" : "/api/rest/v1/test/get-menu"
+    this.apiService.request(url,'get',{},{}).then((data)=>{
+      console.log("data ==>", data);
+      this.menuObject = data.menu;
+    })
+    .catch((error)=>{
+      console.log("error in fetching the json",error);
+    })
+
     this.showLoader = true;
   	let product_slug = this.route.snapshot.paramMap.get('product_slug');
   	console.log("product_slug ==>", product_slug);
 
-  	let url = "https://demo8558685.mockable.io/get_single_product";
+  	url = "https://demo8558685.mockable.io/get_single_product";
   	this.apiService.request(url,'get',{},{}).then((data)=>{
   		this.product = data;
       this.showLoader = false;
