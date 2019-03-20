ea5027dca5ad86694cb78fc488ac35be557e3d1e
Shashank Shetye Saudagar <shashank@ajency.in>
2019-03-20 14:02:21 +0530

    Changes suggested by Sujit

--- app/SingleProduct.php
+++ app/SingleProduct.php
@@ -57,14 +57,15 @@ class SingleProduct
     public function __construct($slug)
     {
         if (is_null(self::$facets)) {
-            self::$facets = Facet::select(['facet_name', 'facet_value', 'display_name', 'slug','sequence'])->get();
+            self::$facets = Facet::select(['facet_name', 'facet_value', 'display_name', 'slug', 'sequence'])->get();
         }
         $this->setSlugElasticData($slug);
         $this->getProductColor();
     }
 
-    private function getProductColor(){
-    	$this->productColor = ProductColor::where('product_id',$this->productData['product_id'])->where('color_id',$this->productData['product_color_id'])->first();
+    private function getProductColor()
+    {
+        $this->productColor = ProductColor::where('product_id', $this->productData['product_id'])->where('color_id', $this->productData['product_color_id'])->first();
     }
 
     private function setSlugElasticData($product_slug)
@@ -144,82 +145,89 @@ class SingleProduct
         return $facets;
     }
 
-    private function getVariantAttributes($variant){
-    	$attributes = [];
+    private function getVariantAttributes($variant)
+    {
+        $attributes = [];
         foreach (self::VARIANT_ATTRIBUTES as $attribute_name) {
             $attributes[$attribute_name] = (isset($variant[$attribute_name])) ? $variant[$attribute_name] : "";
         }
         return $attributes;
     }
 
-    private function getVariantFacets($variant){
-    	$facets = [];
-    	foreach (self::VARIANT_FACETS as $facetName) {
-    		switch ($facetName) {
-    			case 'variant_size':
-    				$facetData          = self::$facets->where('facet_name', 'variant_size_name')->where('facet_value', $variant['variant_size_name'])->first();
-    				$facets[$facetName] = [
-    					'id' => $variant['variant_size_id'],
-    					'name' => $facetData['display_name'],
-    					'slug' => $facetData['slug'],
-    					'sequence' => $facetData['sequence'],
-    				];
-    				break;
-    		}
-    	}
-    	return $facets;
+    private function getVariantFacets($variant)
+    {
+        $facets = [];
+        foreach (self::VARIANT_FACETS as $facetName) {
+            switch ($facetName) {
+                case 'variant_size':
+                    $facetData          = self::$facets->where('facet_name', 'variant_size_name')->where('facet_value', $variant['variant_size_name'])->first();
+                    $facets[$facetName] = [
+                        'id'       => $variant['variant_size_id'],
+                        'name'     => $facetData['display_name'],
+                        'slug'     => $facetData['slug'],
+                        'sequence' => $facetData['sequence'],
+                    ];
+                    break;
+            }
+        }
+        return $facets;
     }
 
-    private function getVariants(){
-    	$variants = [];
-    	$defaultId = defaultVariant($this->variantData);
-    	foreach ($this->variantData as $variant) {
-    		$variantInfo = [];
-    		$variantInfo['attributes'] = $this->getVariantAttributes($variant);
-    		$variantInfo['facets'] = $this->getVariantFacets($variant);
-    		$variantInfo['is_default'] = $defaultId == $variant['variant_id'];
-    		$variants[] = $variantInfo;
-    	}
-    	return $variants;
+    private function getVariants()
+    {
+        $variants  = [];
+        $defaultId = defaultVariant($this->variantData);
+        foreach ($this->variantData as $variant) {
+            $variantInfo                       = [];
+            $variantInfo['variant_attributes'] = $this->getVariantAttributes($variant);
+            $variantInfo['variant_facets']     = $this->getVariantFacets($variant);
+            $variantInfo['is_default']         = $defaultId == $variant['variant_id'];
+            $variants[]                        = $variantInfo;
+        }
+        return $variants;
     }
 
-    private function getImages(){
-    	$images = $this->productColor->getAllImages(['main','thumb','zoom']);
-    	$seq = 1;
-    	foreach ($images as $key => $image) {
-    		$images[$key]['sequence'] = $seq++;
-    	}
-    	return $images;
+    private function getImages()
+    {
+        $images = $this->productColor->getAllImages(['main', 'thumb', 'zoom']);
+        $seq    = 1;
+        foreach ($images as $key => $image) {
+            $images[$key]['sequence'] = $seq++;
+        }
+        return $images;
     }
 
-    private function isSellable(){
-    	return $this->productData['product_att_ecom_sales'];
+    private function isSellable()
+    {
+        return $this->productData['product_att_ecom_sales'];
     }
 
-    private function getColorVariants(){
-    	$colorVariants = [];
-    	$q = new ElasticQuery;
-    	$q->setIndex(config("elastic.indexes.product"));
-    	$productID = $this->productData['product_id'];
-    	$colorId = $this->productData['product_color_id'];
-    	$facetName  = $q::createTerm('search_data.number_facet.facet_name', "product_id");
-	    $facetValue = $q::createTerm('search_data.number_facet.facet_value', $productID);
-	    $must = $q::addToBoolQuery('must', [$facetName, $facetValue]);
-	    $nested = $q::createNested('search_data.number_facet', $must);
-	    $q->setQuery($nested)->setSource(['search_result_data']);
-	    $colorVariantsData = $q->search()['hits']['hits'];
-	    foreach ($colorVariantsData as $cved) {
-	    	$cvd = $cved['_source']['search_result_data'];
-	    	$colorVariant = [];
-	    	$facetData          = self::$facets->where('facet_name', 'product_color_html')->where('facet_value', $cvd['product_color_html'])->first();
-	    	$colorVariant['color_id'] = $cvd['product_color_id'];
-	    	$colorVariant['color_name'] = $facetData['display_name'];
-	    	$colorVariant['image'] = ProductColor::where('product_id',$productID)->where('color_id',$cvd['product_color_id'])->first()->getDefaultImage(["variant-thumb"])["variant-thumb"];
-	    	$colorVariant['url'] = url('/'.$cvd['product_slug'].'/buy');
-	    	$colorVariant['is_selected'] = $cvd['product_color_id'] == $colorId;
-	    	$colorVariants[] = $colorVariant;
-	    }
-	    return $colorVariants;
+    private function getColorVariants()
+    {
+        $colorVariants = [];
+        $q             = new ElasticQuery;
+        $q->setIndex(config("elastic.indexes.product"));
+        $productID  = $this->productData['product_id'];
+        $colorId    = $this->productData['product_color_id'];
+        $facetName  = $q::createTerm('search_data.number_facet.facet_name', "product_id");
+        $facetValue = $q::createTerm('search_data.number_facet.facet_value', $productID);
+        $must       = $q::addToBoolQuery('must', [$facetName, $facetValue]);
+        $nested     = $q::createNested('search_data.number_facet', $must);
+        $q->setQuery($nested)->setSource(['search_result_data']);
+        $colorVariantsData = $q->search()['hits']['hits'];
+        foreach ($colorVariantsData as $cved) {
+            $cvd                         = $cved['_source']['search_result_data'];
+            $colorVariant                = [];
+            $facetData                   = self::$facets->where('facet_name', 'product_color_html')->where('facet_value', $cvd['product_color_html'])->first();
+            $colorVariant['color_id']    = $cvd['product_color_id'];
+            $colorVariant['color_name']  = $facetData['display_name'];
+            $colorVariant['image']       = ProductColor::where('product_id', $productID)->where('color_id', $cvd['product_color_id'])->first()->getDefaultImage(["variant-thumb"])["variant-thumb"];
+            $colorVariant['url']         = url('/' . $cvd['product_slug'] . '/buy');
+            $colorVariant['is_selected'] = $cvd['product_color_id'] == $colorId;
+            $colorVariant['hexcode']     = $facetData['slug'];
+            $colorVariants[]             = $colorVariant;
+        }
+        return $colorVariants;
     }
 
     public function generateSinglePageData($objects)
@@ -234,17 +242,17 @@ class SingleProduct
                     $data['facets'] = $this->getProductFacets();
                     break;
                 case 'variants':
-                	$data['variants'] = $this->getVariants();
-                	break;
+                    $data['variants'] = $this->getVariants();
+                    break;
                 case 'images':
-                	$data['images'] = $this->getImages();
-                	break;
-            	case 'is_sellable':
-            		$data['is_sellable'] = $this->isSellable();
-            		break;
-            	case 'color_variants':
-            		$data['color_variants'] = $this->getColorVariants();
-            		break;
+                    $data['images'] = $this->getImages();
+                    break;
+                case 'is_sellable':
+                    $data['is_sellable'] = $this->isSellable();
+                    break;
+                case 'color_variants':
+                    $data['color_variants'] = $this->getColorVariants();
+                    break;
                 default:
                     throw new \Exception("object type " . $object . " not defined", 1);
 
