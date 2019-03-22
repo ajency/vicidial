0443eace011f9297a0a07669f1383fa5bc735b02
Tanvi Salkar <tanvi.s@ajency.in>
2019-03-22 18:49:45 +0530

    updated FetchProductImages job

--- app/Jobs/FetchProductImages.php
+++ app/Jobs/FetchProductImages.php
@@ -14,7 +14,7 @@ use App\Jobs\GeneratePresetImages;
 class FetchProductImages implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    // public $tries = 3;
+    public $tries = 3;
     protected $productId;
 
     /**
