5e3be4b7f6ae873c9f63aba49088077016df9875
jeromie <jeromie@ajency.in>
2019-03-26 16:04:07 +0530

    uniforms landing

--- kss-pwa-app/src/app/landing-page/landing-page.module.ts
+++ kss-pwa-app/src/app/landing-page/landing-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
     CommonModule,
+    ComponentsModule,
     LandingPageRoutingModule
   ]
 })
