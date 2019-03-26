66df8ff61890d5717bb267eb3c997fe2f1998121
jeromie <jeromie@ajency.in>
2019-03-26 18:51:42 +0530

    banner on home, 7th school added, menu link

--- kss-pwa-app/src/app/landing-page/landing-page/landing-page.component.ts
+++ kss-pwa-app/src/app/landing-page/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
+import { ApiServiceService } from '../../service/api-service.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+	menuObject : any
+
+  constructor(private apiService: ApiServiceService) { }
 
   ngOnInit() {
+
+	  let url = isDevMode() ? "https://demo8558685.mockable.io/get-menu" : "/api/rest/v1/test/get-menu"
+		this.apiService.request(url,'get',{},{}).then((data)=>{
+			console.log("data ==>", data);
+			this.menuObject = data.menu;
+		})
+		.catch((error)=>{
+			console.log("error in fetching the json",error);
+		})
   }
 
 }
