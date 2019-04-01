e2e79f1745b2517b27cff3003439a58234028d89
Saeesh <saeesh@ajency.in>
2019-04-01 15:51:48 +0530

    Minor changes with code

--- app/Variant.php
+++ app/Variant.php
@@ -157,12 +157,12 @@ class Variant extends Model
     public function getItem($related_items = true, $current_quantity = false)
     {
         $item = array(
-            'product_slug'       => $this->getProductSlug(),
-            'availability'       => $this->getAvailability(),
-            'message'            => $this->getMessage(),
-            'attributes'         => $this->getItemAttributes(),
-            "id"                 => $this->id,
-            "pixel_id"           => $this->getParentId() . "-" . $this->getVarColorId(),
+            'product_slug' => $this->getProductSlug(),
+            'availability' => $this->getAvailability(),
+            'message'      => $this->getMessage(),
+            'attributes'   => $this->getItemAttributes(),
+            "id"           => $this->id,
+            "pixel_id"     => $this->getParentId() . "-" . $this->getVarColorId(),
         );
 
         if ($related_items) {
@@ -175,7 +175,7 @@ class Variant extends Model
 
         return $item;
     }
-    
+
     public function getItemAttributes()
     {
 
@@ -405,8 +405,6 @@ class Variant extends Model
     {
         UpdateVariantInventory::dispatch($params['variants'])->onQueue('update_inventory');
     }
-
-
 
     public static function getWarehouseInventory()
     {
