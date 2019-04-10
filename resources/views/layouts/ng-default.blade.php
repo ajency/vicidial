a65c9c5d3b97bc489b9672d07d22622ef848fd3f
sujitgawas19 <sujit@ajency.in>
2019-04-10 16:32:54 +0530

    added get cookie function

--- resources/views/layouts/ng-default.blade.php
+++ resources/views/layouts/ng-default.blade.php
@@ -22,7 +22,23 @@
 	  fbq('init', '{{config("analytics.pixel_id")}}');
 	  fbq('track', 'PageView');
 
-	  function fbTrackViewContent(default_price, parent_id, selected_color_id){
+	  function getCookie(cname) {
+	    var name = cname + "=";
+	    var decodedCookie = decodeURIComponent(document.cookie);
+	    var ca = decodedCookie.split(';');
+	    for(var i = 0; i < ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) == ' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length, c.length);
+	        }
+	    }
+	    return "";
+	  }
+
+	function fbTrackViewContent(default_price, parent_id, selected_color_id){
 	    console.log("fbTrackViewContent ==>", default_price, parent_id, selected_color_id);
 	    fbq('track', 'ViewContent', {
 	        value: default_price,
