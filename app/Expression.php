919efeec237ea55df12f170aee6a997d2d97ab86
Saeesh <saeesh@ajency.in>
2019-04-23 12:25:47 +0530

    added expression for returns

--- app/Expression.php
+++ app/Expression.php
@@ -1,35 +1,37 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Expression extends Model
 {
-	protected $casts = [
-		'value' => 'array',
-	];
+    protected $casts = [
+        'value' => 'array',
+    ];
     public function parent()
     {
         return $this->morphTo();
     }
-
-    public function validate($cartData){
-    	switch($this->filter){
-    		case 'greater_than':
-    			switch ($this->entity) {
-    				case 'cart_price':
-    					return ($cartData['final_total'] >= $this->value[0]);
-    					break;
-    				
-    				default:
-    					return false;
-    					break;
-    			}
-    			break;
-    		default:
-    			return false;
-    			break;
-    	}
+    public function validate($data){
+        switch($this->filter){
+            case 'greater_than':
+                switch ($this->entity) {
+                    case 'cart_price':
+                        return ($data['final_total'] >= $this->value[0]);
+                        break;      
+                    default:
+                        return false;
+                        break;
+                }
+                break;
+            case 'less_than':        
+                switch ($this->entity) {
+                    case 'days':
+                        return ($data['days'] <= $this->value[0]);
+                        break;          
+                }
+                break; 
+            default:
+                return false;
+                break;
+        }
     }
 }
