25fa3ba83539e90380f8c040eb702cd2d9000def
sujitgawas19 <sujit@ajency.in>
2019-04-29 15:38:07 +0530

    scroll to view based on hash fragments

--- kss-pwa-app/src/app/terms-and-conditions/terms-and-conditions/terms-and-conditions.component.ts
+++ kss-pwa-app/src/app/terms-and-conditions/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.component.html',
@@ -12,9 +12,22 @@ export class TermsAndConditionsComponent implements OnInit {
     {position: 2, title: 'Terms and Conditions', url: ''},
   ];
 
-  constructor() { }
+  @ViewChild('shipping') shipping : ElementRef;
+  @ViewChild('return') return : ElementRef;
+  @ViewChild('cancellation') cancellation : ElementRef;
+
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => { 
+      console.log(fragment);
+      if(fragment == 'shipping')
+        this.shipping.nativeElement.scrollIntoView()
+      if(fragment == 'return')
+        this.return.nativeElement.scrollIntoView()
+      if(fragment == 'cancellation')
+        this.cancellation.nativeElement.scrollIntoView()
+    });
   }
 
 }
