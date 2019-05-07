dfac6d4da5bebe86868c02ec4df440badd6722a4
sujitgawas19 <sujit@ajency.in>
2019-05-07 17:42:59 +0530

    removed blade caching

--- workbox-config.js
+++ workbox-config.js
@@ -4,10 +4,10 @@ module.exports = {
     // "views/kss-pwa/*.{css,ico,eot,svg,ttf,woff,woff2,js,json}",
     "js/kss-pwa/*.{css,js,json}",
     "manifest.json"
-  ],
-  "templatedUrls" : {
-        '/': ['../resources/views/layouts/ng-default.blade.php']
-      },
+  ]
+  // "templatedUrls" : {
+  //       '/': ['../resources/views/layouts/ng-default.blade.php']
+  //     },
   "swDest": "public/service-worker.js",
   "swSrc": "service-worker-src.js"
 };
