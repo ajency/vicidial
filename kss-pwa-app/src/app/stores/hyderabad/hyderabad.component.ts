2eb6e23f82a91543b44f9d8eca3d9e81113a2717
sujitgawas19 <sujit@ajency.in>
2019-05-16 12:04:11 +0530

    cdn path for stores images

--- kss-pwa-app/src/app/stores/hyderabad/hyderabad.component.ts
+++ kss-pwa-app/src/app/stores/hyderabad/hyderabad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from '../../service/app-service.service';
 
 @Component({
   selector: 'app-hyderabad',
@@ -6,33 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hyderabad.component.scss']
 })
 export class HyderabadComponent implements OnInit {
+  storeImages = [];
+  cdnUrl : any;
 
-  storeImages = [
-    {
-        small: '../../../assets/img/stores/hyderabad/hyderabad3-large.jpg',
-        medium: '../../../assets/img/stores/hyderabad/hyderabad3-small.jpg',
-        big: '../../../assets/img/stores/hyderabad/hyderabad3-large.jpg'
-    },
-    {
-        small: '../../../assets/img/stores/hyderabad/hyderabad1-large.jpg',
-        medium: '../../../assets/img/stores/hyderabad/hyderabad1-small.jpg',
-        big: '../../../assets/img/stores/hyderabad/hyderabad1-large.jpg'
-    },
-    {
-        small: '../../../assets/img/stores/hyderabad/hyderabad2-large.jpg',
-        medium: '../../../assets/img/stores/hyderabad/hyderabad2-small.jpg',
-        big: '../../../assets/img/stores/hyderabad/hyderabad2-large.jpg'
-    },
-    {
-        small: '../../../assets/img/stores/hyderabad/hyderabad14-large.jpg',
-        medium: '../../../assets/img/stores/hyderabad/hyderabad14-small.jpg',
-        big: '../../../assets/img/stores/hyderabad/hyderabad14-large.jpg'
-    },    
-  ];
-
-  constructor() { }
+  constructor(private appservice : AppServiceService) { }
 
   ngOnInit() {
+    this.cdnUrl = this.appservice.cdnUrl;
+    this.storeImages = [
+      {
+          small: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad3-large.jpg',
+          medium: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad3-small.jpg',
+          big: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad3-large.jpg'
+      },
+      {
+          small: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad1-large.jpg',
+          medium: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad1-small.jpg',
+          big: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad1-large.jpg'
+      },
+      {
+          small: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad2-large.jpg',
+          medium: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad2-small.jpg',
+          big: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad2-large.jpg'
+      },
+      {
+          small: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad14-large.jpg',
+          medium: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad14-small.jpg',
+          big: this.cdnUrl + 'assets/img/stores/hyderabad/hyderabad14-large.jpg'
+      },    
+    ];
   }
 
 }
