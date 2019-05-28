69e6f7ddeff84a5d40af8d5323df3e238a256cfa
sujitgawas19 <sujit@ajency.in>
2019-05-28 16:31:12 +0530

    display specific products

--- kss-pwa-app/src/app/shared-components/promotions/promotions-list/promotions-list.component.ts
+++ kss-pwa-app/src/app/shared-components/promotions/promotions-list/promotions-list.component.ts
@@ -13,6 +13,7 @@ export class PromotionsListComponent implements OnInit, OnChanges {
 	@Input() promotionsList : any;
 	@Input() orderTotal : any;
   @Input() appliedCoupon : any;
+  @Input() items : any;
 
 	applicablePromotions : any;
 	nonApplicablePromotions : any;
@@ -20,40 +21,69 @@ export class PromotionsListComponent implements OnInit, OnChanges {
 
   ngOnInit() {
   	// console.log("ngOnInit", this.promotionsList, this.orderTotal);
-	  	this.updatePromotionsData();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // console.log("ngOnChanges", this.promotionsList, this.orderTotal);
+  ngOnChanges() {
     this.updatePromotionsData();
-    // this.orderTotal = 1000;
   }
 
   updatePromotionsData(){
-    this.promotionsList = this.promotionsList.filter((promo)=>{ return (promo.condition.entity == 'cart_price' && promo.condition.filter == 'greater_than') });
-
+    this.promotionsList = this.promotionsList.filter((promo)=>{ return (promo.condition.filter == 'greater_than') });
     try{
       this.promotionsList.forEach((promo)=>{ 
         promo.actual_discount = this.appservice.calculateDiscount(promo.action.type, promo.action.value, this.orderTotal);
-        console.log(promo.actual_discount);
       });
     }
     catch(e){
       console.log("error ==>",e);
     }
-  	this.promotionsList =  this.appservice.sortArray(this.promotionsList);
-  	console.log(this.promotionsList);
-		// this.calculateAge();
-  	let obj = this.appservice.filterArray(this.promotionsList, this.orderTotal);
-  	this.applicablePromotions = obj.applicable;
-  	this.nonApplicablePromotions = obj.non_applicable;
-    console.log("filtered array ==>", obj);
+
+    this.applicablePromotions = [];
+    this.nonApplicablePromotions = [];
+    this.promotionsList.forEach((promo)=>{
+      switch(promo.condition.entity){
+        case 'cart_price' : 
+          if(promo.condition.value[0] <= this.orderTotal)
+            this.applicablePromotions.push(promo);
+          else
+            this.nonApplicablePromotions.push(promo);
+          break;
+
+        case 'specific_products' :
+          if(this.productSpecificCouponApplicable(promo))
+            this.applicablePromotions.push(promo);
+          else
+            this.nonApplicablePromotions.push(promo);
+          break;
+      }  
+    })
+  }
+
+  productSpecificCouponApplicable(promo){
+    let facet = promo.condition.value.facet;
+    let price = 0;
+    if(promo.condition.value.activity == 'include'){
+      this.items.forEach((item)=>{
+        if( (!facet.age_group || item.age_group === facet.age_group) && (!facet.category_type || item.category_type === facet.category_type) && (!facet.gender || item.gender === facet.gender) && (!facet.subtype || facet.subtype === item.subtype) && !promo.condition.value.variant.includes(item.odoo_id) ){
+          price = price + item.price_final;
+        }
+      })
+    }
+    else if(promo.condition.value.activity == 'exclude'){
+      this.items.forEach((item)=>{
+        if( (!facet.age_group || item.age_group !== facet.age_group) && (!facet.category_type || item.category_type !== facet.category_type) && (!facet.gender || item.gender !== facet.gender) && (!facet.subtype || facet.subtype !== item.subtype) && promo.condition.value.variant.includes(item.odoo_id) ){
+          price = price + item.price_final;
+        }
+      })
+    }
+    if(price > promo.condition.value.value)
+      return true;
+    return false;
   }
 
   calculateAge(){
   	this.promotionsList.forEach((promotion)=>{
   		promotion.age = this.getAge(promotion.valid_from);
-  		// console.log(promotion.age);
   	})
   }
 
