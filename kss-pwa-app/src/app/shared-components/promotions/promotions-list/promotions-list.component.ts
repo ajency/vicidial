0c77925f68fff71d715595a5387c8eddcf4f4dfa
sujitgawas19 <sujit@ajency.in>
2019-05-28 17:17:13 +0530

    product specific coupon — item price * quantity

--- kss-pwa-app/src/app/shared-components/promotions/promotions-list/promotions-list.component.ts
+++ kss-pwa-app/src/app/shared-components/promotions/promotions-list/promotions-list.component.ts
@@ -65,14 +65,14 @@ export class PromotionsListComponent implements OnInit, OnChanges {
     if(promo.condition.value.activity == 'include'){
       this.items.forEach((item)=>{
         if( (!facet.age_group || item.age_group === facet.age_group) && (!facet.category_type || item.category_type === facet.category_type) && (!facet.gender || item.gender === facet.gender) && (!facet.subtype || facet.subtype === item.subtype) && !promo.condition.value.variant.includes(item.odoo_id) ){
-          price = price + item.price_final;
+          price = price + item.attributes.price_final * item.quantity;
         }
       })
     }
     else if(promo.condition.value.activity == 'exclude'){
       this.items.forEach((item)=>{
         if( (!facet.age_group || item.age_group !== facet.age_group) && (!facet.category_type || item.category_type !== facet.category_type) && (!facet.gender || item.gender !== facet.gender) && (!facet.subtype || facet.subtype !== item.subtype) && promo.condition.value.variant.includes(item.odoo_id) ){
-          price = price + item.price_final;
+          price = price + item.attributes.price_final * item.quantity;
         }
       })
     }
