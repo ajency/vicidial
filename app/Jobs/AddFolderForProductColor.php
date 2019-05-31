fb1721d9e595d299f38ce74936fc16d786da967a
Viraj Verlekar <viraj@ajency.in>
2019-05-31 12:32:51 +0530

    pass product id

--- app/Jobs/AddFolderForProductColor.php
+++ app/Jobs/AddFolderForProductColor.php
@@ -33,8 +33,8 @@ class AddFolderForProductColor implements ShouldQueue
         $elastic_data = $this->product_color->getElasticData();
         $attributes = collect($elastic_data['search_data'][0]['attributes'])->keyBy('attribute_name');
         \Ajency\ServiceComm\Comm\Async::call('NewProductColor', [
-            'external_product_id' => $this->product_color->id,
-            'product_color_id'    => $this->product_color->color_id,
+            'external_product_id' => $this->product_color->product_id,
+            'product_color_id'    => $this->product_color->id,
             'product_barcode'     => (isset($attributes['product_barcode']['attribute_value'])) ? $attributes['product_barcode']['attribute_value'] : '',
             'product_name'        => $elastic_data['search_result_data']['product_title'],
             'product_color'       => $elastic_data['search_result_data']['product_color_name'] . "-" . str_slug($elastic_data['search_result_data']['product_color_html']),
