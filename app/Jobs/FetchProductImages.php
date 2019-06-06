10a379f2efacbf71379c4d35aaec1d2ffa123a0f
Viraj Verlekar <viraj@ajency.in>
2019-06-07 02:26:18 +0530

    fetch temporary image from s3

--- app/Jobs/FetchProductImages.php
+++ app/Jobs/FetchProductImages.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Jobs\GeneratePresetImages;
 use App\Product;
 use App\ProductColor;
 use Illuminate\Bus\Queueable;
@@ -9,7 +10,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Jobs\GeneratePresetImages;
 
 class FetchProductImages implements ShouldQueue
 {
@@ -36,7 +36,7 @@ class FetchProductImages implements ShouldQueue
     public function handle()
     {
         $productColorId = $this->payload['product_color_id'];
-        $prod_images = $this->payload['prod_images'];
+        $prod_images    = $this->payload['prod_images'];
         \Log::debug("debugging for processing images from google drive");
         \Log::debug("count of images from google driv for product color id");
         \Log::debug($productColorId);
@@ -46,30 +46,30 @@ class FetchProductImages implements ShouldQueue
         $productColor = ProductColor::find($productColorId);
         $productColor->unmapAllImages();
 
-        if($prod_images->count() == 0) {
-            ProductColor::where('id', $productColorId)->update(['no_image'=>true]);
+        if (count($prod_images) == 0) {
+            ProductColor::where('id', $productColorId)->update(['no_image' => true]);
             return;
-        }else{
-            ProductColor::where('id', $productColorId)->update(['no_image'=>false]);
+        } else {
+            ProductColor::where('id', $productColorId)->update(['no_image' => false]);
         }
 
-        $extension   = "jpg";
+        $extension = "jpg";
 
-        $prod_images_arr   = json_decode($prod_images, true);
-        $colors            = array_column($prod_images_arr, "color_name");
-        $default_color_ids = [];
-        $db_image_ids=[];
+        $prod_images_arr       = $prod_images;
+        $colors                = array_column($prod_images_arr, "color_name");
+        $default_color_ids     = [];
+        $db_image_ids          = [];
         $product_color_details = [];
         foreach ($prod_images as $pIndex => $prodImage) {
             $pc            = ProductColor::find($productColorId);
             $image         = $prodImage['image'];
-            $product_name  = ($prodImage["magento_name"] == "") ? $prodImage["magento_name"] : $prodImage["name"];
-            $color_name = isset($prodImage["color_name"])?$prodImage["color_name"]:"";
-            $imageName     = generateVariantImageName($product_name, $color_name, $colors,$pIndex);
+            $product_name  = $prodImage["name"];
+            $color_name    = isset($prodImage["color_name"]) ? $prodImage["color_name"] : "";
+            $imageName     = generateVariantImageName($product_name, $color_name, $colors, $pIndex);
             $imageFullName = $imageName . "." . $extension;
             $subfilepath   = '/variants/' . $imageFullName;
             $subpath       = 'variants/' . $imageFullName;
-            $actualImage   = file_get_contents($image);
+            $actualImage   = \Storage::disk('s3')->get($image);
             \Storage::put($subfilepath, $actualImage);
             $disk       = \Storage::disk('local');
             $filepath   = ($disk->getDriver()->getAdapter()->getPathPrefix()) . $subpath;
@@ -86,17 +86,24 @@ class FetchProductImages implements ShouldQueue
             array_push($db_image_ids, $image_id);
             $pc->mapImage($image_id, $type);
             // \Storage::disk('local')->delete($subfilepath);
-            if(!isset($product_color_details[$pc->id]))
+            if (!isset($product_color_details[$pc->id])) {
                 $product_color_details[$pc->id] = [];
-            array_push($product_color_details[$pc->id] , ["photo_id"=>$image_id,"filename"=>$imageFullName]);
+            }
+
+            array_push($product_color_details[$pc->id], ["photo_id" => $image_id, "filename" => $imageFullName]);
 
         }
         Product::updateImageFacets($productColorId);
-        GeneratePresetImages::dispatch($productColorId,$product_color_details)->onQueue('process_product_image_presets');
+        GeneratePresetImages::dispatch($productColorId, $product_color_details)->onQueue('process_product_image_presets');
         \Log::debug("count of images after processing to DB for product id");
         \Log::debug($productColorId);
         \Log::debug("=");
         \Log::debug($db_image_ids);
-        
+
+        foreach ($prod_images as $pIndex => $prodImage) {
+            $image = $prodImage['image'];
+            \Storage::delete($image);
+        }
+
     }
 }
