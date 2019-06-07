a90c8bcb3082e80dfa65d965a3c0d08fd7c6c625
Viraj Verlekar <viraj@ajency.in>
2019-06-07 10:28:01 +0530

    Add timestamp to images

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -439,8 +439,7 @@ function generateVariantImageName($product_name, $color_name, $colors, $index)
         $append = ($index + 1);
     }
 
-    $image_name = $product_name . "-" . $color_name . $append;
-    $image_name = str_slug(implode(' ', [$product_name, $color_name, $append]));
+    $image_name = str_slug(implode(' ', [$product_name, $color_name, $append, Carbon::now()->timestamp]));
     return $image_name;
 
 }
