73fd272ab33af5d40e9da55a3367df1ed0b28232
Saeesh <saeesh@ajency.in>
2019-06-21 11:58:47 +0530

    payu_payments migration file

---
+++ database/migrations/2019_06_21_053519_change_mihpayid_in_payu_payments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeMihpayidInPayuPayments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payu_payments', function (Blueprint $table) {
+             $table->string('mihpayid')->nullable(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payu_payments', function (Blueprint $table) {
+            //
+        });
+    }
+}
