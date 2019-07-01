fc2ac1d0907966112404ab745558f0193ab701c4
Saeesh <saeesh@ajency.in>
2019-07-01 14:38:32 +0530

    config fix for notify job

--- app/Jobs/NotifyPayment.php
+++ app/Jobs/NotifyPayment.php
@@ -54,75 +54,75 @@ class NotifyPayment implements ShouldQueue
                     'unmappedstatus' => 'pending',
                     'bankcode'       => $request_params['bankcode'],
                 ]);
-
-                if (config('app.env') == 'production') {
-                    $post_params = [
-                        'merchantKey'            => config('payu.payumoney.key'),
-                        'merchantTransactionIds' => $request_params['merchantTransactionId'],
-                    ];
-                    $api_url  = config('payu.paymentResponseApiUrl') . '?' . http_build_query($post_params);
-                    $client   = new Client();
-                    $response = $client->request('POST', $api_url, [
-                        'headers' => [
-                            'Authorization' => config('payu.payumoney.auth'),
-                        ],
-                    ]);
-                    $response_params = json_decode($response->getBody(), true);
-                    if (isset($response_params['result'][0]['postBackParam'])) {
-                        $post_back_params               = $response_params['result'][0]['postBackParam'];
-                        $payu_payment->data             = json_encode($response_params['result']);
-                        $payu_payment->bank_ref_num     = $post_back_params['bank_ref_num'];
-                        $payu_payment->bankcode         = $post_back_params['bankcode'];
-                        $payu_payment->cardnum          = $post_back_params['cardnum'];
-                        $payu_payment->name_on_card     = $post_back_params['name_on_card'];
-                        $payu_payment->card_type        = $post_back_params['card_type'];
-                        $payu_payment->mihpayid         = $post_back_params['mihpayid'];
-                        $payu_payment->unmappedstatus   = $post_back_params['unmappedstatus'];
-                        $payu_payment->net_amount_debit = $post_back_params['net_amount_debit'];
-                        $payu_payment->save();
-                    }
-                }
-
-                try {
-                    if ($this->status == 'success') {
-                        $order->status           = 'payment-successful';
-                        $order->transaction_mode = 'Prepaid';
-                        $order->save();
-                        $order->placeOrderOnOdoo();
-                        $cart       = $order->cart;
-                        $cart->type = 'order-complete';
-                        $cart->save();
-                        $order->sendSuccessEmail();
-                        $order->sendSuccessSMS();
-                        $order->sendVendorSMS();
-                    } else {
-                        $order->status = 'payment-failed';
-                        $order->save();
-                        $order->updateInventory('UnreserveInventory');
-                    }
-
-                } catch (\Exception $e) {
-                    \Log::notice('Order Success Payu Method Failed with error: ' . $e->getMessage());
-                    \Log::notice('Order id : ' . $order->id);
-                    sendEmail('failed-job', [
-                        'from'          => config('communication.failed-job.from'),
-                        'subject'       => 'Order Success Method Failed : PAYU [' . config('app.env') . ']',
-                        'template_data' => [
-                            'queue'     => 'Order Success Method',
-                            'job'       => 'Order Success Method',
-                            'exception' => $e->getMessage(),
-                            'body'      => 'Order id : ' . $order->id,
-                            'trace'     => $e->getTraceAsString(),
-                        ],
-                        'priority'      => 'default',
-                    ]);
-                }
-                $order->updateOrderlineIndex(['status', 'transaction_mode']);
-                $order->payment_in_progress = false;
-                $order->save();
             } catch (\Exception $e) {
                 \Log::notice('payu_payment update failed for order ID:' . $order->id . ' with error: ' . $e->getMessage());
             }
+            if (config('app.env') == 'production') {
+                $post_params = [
+                    'merchantKey'            => config('payu.accounts.payumoney.key'),
+                    'merchantTransactionIds' => $request_params['merchantTransactionId'],
+                ];
+                $api_url = config('payu.paymentResponseApiUrl') . '?' . http_build_query($post_params);
+                $client   = new Client();
+                $response = $client->request('POST', $api_url, [
+                    'headers' => [
+                        'Authorization' => config('payu.accounts.payumoney.auth'),
+                    ],
+                ]);
+
+                $response_params = json_decode($response->getBody(), true);
+                if (isset($response_params['result'][0]['postBackParam'])) {
+                    $post_back_params               = $response_params['result'][0]['postBackParam'];
+                    $payu_payment->data             = json_encode($response_params['result']);
+                    $payu_payment->bank_ref_num     = $post_back_params['bank_ref_num'];
+                    $payu_payment->bankcode         = $post_back_params['bankcode'];
+                    $payu_payment->cardnum          = $post_back_params['cardnum'];
+                    $payu_payment->name_on_card     = $post_back_params['name_on_card'];
+                    $payu_payment->card_type        = $post_back_params['card_type'];
+                    $payu_payment->mihpayid         = $post_back_params['mihpayid'];
+                    $payu_payment->unmappedstatus   = $post_back_params['unmappedstatus'];
+                    $payu_payment->net_amount_debit = $post_back_params['net_amount_debit'];
+                    $payu_payment->save();
+                }
+            }
+
+            try {
+                if ($this->status == 'success') {
+                    $order->status           = 'payment-successful';
+                    $order->transaction_mode = 'Prepaid';
+                    $order->save();
+                    $order->placeOrderOnOdoo();
+                    $cart       = $order->cart;
+                    $cart->type = 'order-complete';
+                    $cart->save();
+                    $order->sendSuccessEmail();
+                    $order->sendSuccessSMS();
+                    $order->sendVendorSMS();
+                } else {
+                    $order->status = 'payment-failed';
+                    $order->save();
+                    $order->updateInventory('UnreserveInventory');
+                }
+
+            } catch (\Exception $e) {
+                \Log::notice('Order Success Payu Method Failed with error: ' . $e->getMessage());
+                \Log::notice('Order id : ' . $order->id);
+                sendEmail('failed-job', [
+                    'from'          => config('communication.failed-job.from'),
+                    'subject'       => 'Order Success Method Failed : PAYU [' . config('app.env') . ']',
+                    'template_data' => [
+                        'queue'     => 'Order Success Method',
+                        'job'       => 'Order Success Method',
+                        'exception' => $e->getMessage(),
+                        'body'      => 'Order id : ' . $order->id,
+                        'trace'     => $e->getTraceAsString(),
+                    ],
+                    'priority'      => 'default',
+                ]);
+            }
+            $order->updateOrderlineIndex(['status', 'transaction_mode']);
+            $order->payment_in_progress = false;
+            $order->save();
         }
     }
 }
