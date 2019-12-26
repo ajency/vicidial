332f240c9ac2688804aff82793c586105449b619
Saeesh <saeesh@ajency.in>
2019-12-26 10:45:59 +0530

    fetch call logs data

--- app/Vicidial.php
+++ app/Vicidial.php
@@ -4,11 +4,11 @@ namespace App;
 
 class Vicidial
 {
-    private $mapping = [
+    private static $mapping = [
         'agent'    => [
             'name'           => 'vicidial_users.full_name',
             'id'             => 'vicidial_users.user_id',
-            'login_status'   => 'vicidial_agent_log.sub_status',
+            'login_status'   => '', //'vicidial_agent_log.sub_status',
             'last_activity'  => '',
             'current_status' => '',
         ],
@@ -69,7 +69,26 @@ class Vicidial
 
     public static function fetch()
     {
-   
+    	foreach(collect($data)->flatten()->filter()->values() as $field){
+    		$field_arr = explode('.', $field); 
+    		$fields_by_table[$field_arr[0]][] = $field;
+    	}
+
+        $log = \DB::connection('vicidial')->table('vicidial_log')->get();
+        collect($log)->chunk(10)->each(function($chunked_log) use ($fields_by_table){
+        	$fields_by_table = collect($fields_by_table);
+        	$campaign_ids = collect($chunked_log)->pluck('campaign_id');
+        	$list_ids = collect($chunked_log)->pluck('list_id');
+        	$lead_ids = collect($chunked_log)->pluck('lead_id');
+        	$status_ids = collect($chunked_log)->pluck('status');
+
+        	$campaigns = \DB::connection('vicidial')->table('vicidial_campaigns')->whereIn('campaign_id', $campaign_ids)->get($field_by_table['vicidial_campaigns']);
+        	$lists = \DB::connection('vicidial')->table('vicidial_lists')->whereIn('list_id', $list_ids)->get($field_by_table['vicidial_lists']);
+        	$leads = \DB::connection('vicidial')->table('vicidial_list')->whereIn('lead_id', $lead_ids)->get($field_by_table['vicidial_list']);
+        	$statuses = \DB::connection('vicidial')->table('vicidial_statuses')->whereIn('status', $status_ids)->get($field_by_table['vicidial_statuses']);
+        	$response_data = (object)array_merge_recursive((array)$chunked_log, (array)$campaigns, (array)$lists, (array)$leads, (array)$statuses);
+        });
+        return $response_data;
     }
 
     public static function sanitze()
