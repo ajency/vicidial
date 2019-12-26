1ed77023693fc82bb8a2931acc138c8c866feb07
Saeesh <saeesh@ajency.in>
2019-12-26 13:35:35 +0530

    config('field_mapping')

--- app/Vicidial.php
+++ app/Vicidial.php
@@ -6,69 +6,6 @@ use App\Jobs\IndexData;
 
 class Vicidial
 {
-    private static $mapping = [
-        'agent'    => [
-            'name'           => 'vicidial_users.full_name',
-            'id'             => 'vicidial_users.user_id',
-            'login_status'   => '', //'vicidial_agent_log.sub_status',
-            'last_activity'  => '',
-            'current_status' => '',
-        ],
-        'session'  => [
-            //         'station' => 'vicidial_stations.agent_station',
-            'id' => 'vicidial_session_data.session_name',
-        ],
-        'call'     => [
-            'id'                   => 'vicidial_log.uniqueid',
-            'wait_time'            => '',
-            'start_time'           => 'vicidial_log.start_epoch',
-            'end_time'             => 'vicidial_log.end_epoch',
-            'lead_status'          => 'vicidial_list.status',
-            'desposition_assigned' => 'vicidial_log.status',
-            'desposition_value'    => 'vicidial_statuses.status_name',
-            'contact'              => '',
-            'active_time_spent'    => 'vicidial_log.length_in_sec',
-            'call_status'          => 'vicidial_log.status',
-            'lasts_sync'           => '',
-        ],
-        'lead'     => [
-            'id'                => 'vicidial_log.lead_id',
-            'country_code'      => 'vicidial_list.country_code',
-            'phone'             => 'vicidial_list.phone_number',
-            'first_name'        => 'vicidial_list.first_name',
-            'middle_name'       => 'vicidial_list.middle_initial',
-            'last_name'         => 'vicidial_list.last_name',
-            'owner'             => 'vicidial_list.owner',
-            'rank'              => 'vicidial_list.rank',
-            'invalid'           => '',
-            'valid'             => '',
-            'non-british'       => '',
-            'dialable'          => '',
-            'quantity_eu'       => '',
-            'lead-subscription' => '',
-            'clean-lines'       => '',
-            'dial_type'         => 'vicidial_log.alt_dial',
-        ],
-        'campaign' => [
-            'name'            => 'vicidial_campaigns.campaign_name',
-            'id'              => 'vicidial_campaigns.campaign_id',
-            'dial_level'      => 'vicidial_campaigns.auto_dial_level',
-            'hopper_setting'  => 'vicidial_campaigns.hopper_level',
-            'no_hopper_leads' => 'vicidial_campaigns.no_hopper_leads_logins',
-            'lead_filter'     => 'vicidial_campaigns.lead_filter_id',
-            'dial_method'     => '',
-            'list_order'      => 'vicidial_campaigns.lead_order',
-        ],
-        'list'     => [
-            'name'                 => 'vicidial_lists.list_name',
-            'id'                   => 'vicidial_lists.list_id',
-            'status'               => 'vicidial_lists.active',
-            'expiration_date'      => 'vicidial_lists.expiration_date',
-            'total_number_records' => '',
-            'duplicate_records'    => '',
-        ],
-    ];
-
     public static function fetch()
     {
         $data = \DB::connection('vicidial')->table('vicidial_log')
@@ -81,7 +18,7 @@ class Vicidial
                 $join->on('vicidial_session_data.user', '=', 'vicidial_log.user')
                     ->on('vicidial_session_data.campaign_id', '=', 'vicidial_log.campaign_id');
             })
-            ->select(collect(self::$mapping)->flatten()->filter()->values()->map(function ($field) {
+            ->select(collect(config('field_mapping'))->flatten()->filter()->values()->map(function ($field) {
                 return $field . ' as ' . $field;
             })->toArray())
             ->get();
@@ -93,11 +30,14 @@ class Vicidial
     {
         $sanitized_data = collect();
         foreach ($raw_data as $single_data) {
-            $mapping = self::$mapping;
+            $mapping = config('field_mapping');
             foreach ($mapping as $entity => &$entity_data) {
                 foreach ($entity_data as $name => &$field) {
                     if ($field) {
-                        $field = $single_data->{$field};
+                        if ($name) {
+                            $field = $single_data->{$field};
+                        }
+
                     }
                 }
             }
---
+++ config/field_mapping.php
@@ -0,0 +1,207 @@
+<?php
+return [
+    'agent'    => [
+        'name'           => [
+            'field' => 'vicidial_users.full_name',
+            'type'  => 'string',
+        ],
+        'id'             => [
+            'field' => 'vicidial_users.user_id',
+            'type'  => 'string',
+        ],
+        'login_status'   => [
+            'field' => '', //'vicidial_agent_log.sub_status',
+            'type'  => 'string',
+        ],
+        'last_activity'  => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'current_status' => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+    ],
+    'session'  => [
+        /*'station'  => [
+            'field' => 'vicidial_stations.agent_station',
+            'type'  => 'string',
+        ],*/
+        'id' => [
+            'field' => 'vicidial_session_data.session_name',
+            'type'  => 'string',
+        ],
+    ],
+    'call'     => [
+        'id'                   => [
+            'field' => 'vicidial_log.uniqueid',
+            'type'  => 'string',
+        ],
+        'wait_time'            => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'start_time'           => [
+            'field' => 'vicidial_log.start_epoch',
+            'type'  => 'string',
+        ],
+        'end_time'             => [
+            'field' => 'vicidial_log.end_epoch',
+            'type'  => 'string',
+        ],
+        'lead_status'          => [
+            'field' => 'vicidial_list.status',
+            'type'  => 'string',
+        ],
+        'desposition_assigned' => [
+            'field' => 'vicidial_log.status',
+            'type'  => 'string',
+        ],
+        'desposition_value'    => [
+            'field' => 'vicidial_statuses.status_name',
+            'type'  => 'string',
+        ],
+        'contact'              => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'active_time_spent'    => [
+            'field' => 'vicidial_log.length_in_sec',
+            'type'  => 'string',
+        ],
+        'call_status'          => [
+            'field' => 'vicidial_log.status',
+            'type'  => 'string',
+        ],
+        'lasts_sync'           => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+    ],
+    'lead'     => [
+        'id'                => [
+            'field' => 'vicidial_log.lead_id',
+            'type'  => 'string',
+        ],
+        'country_code'      => [
+            'field' => 'vicidial_list.country_code',
+            'type'  => 'string',
+        ],
+        'phone'             => [
+            'field' => 'vicidial_list.phone_number',
+            'type'  => 'string',
+        ],
+        'first_name'        => [
+            'field' => 'vicidial_list.first_name',
+            'type'  => 'string',
+        ],
+        'middle_name'       => [
+            'field' => 'vicidial_list.middle_initial',
+            'type'  => 'string',
+        ],
+        'last_name'         => [
+            'field' => 'vicidial_list.last_name',
+            'type'  => 'string',
+        ],
+        'owner'             => [
+            'field' => 'vicidial_list.owner',
+            'type'  => 'string',
+        ],
+        'rank'              => [
+            'field' => 'vicidial_list.rank',
+            'type'  => 'string',
+        ],
+        'invalid'           => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'valid'             => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'non-british'       => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'dialable'          => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'quantity_eu'       => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'lead-subscription' => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'clean-lines'       => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'dial_type'         => [
+            'field' => 'vicidial_log.alt_dial',
+            'type'  => 'string',
+        ],
+    ],
+    'campaign' => [
+        'name'            => [
+            'field' => 'vicidial_campaigns.campaign_name',
+            'type'  => 'string',
+        ],
+        'id'              => [
+            'field' => 'vicidial_campaigns.campaign_id',
+            'type'  => 'string',
+        ],
+        'dial_level'      => [
+            'field' => 'vicidial_campaigns.auto_dial_level',
+            'type'  => 'string',
+        ],
+        'hopper_setting'  => [
+            'field' => 'vicidial_campaigns.hopper_level',
+            'type'  => 'string',
+        ],
+        'no_hopper_leads' => [
+            'field' => 'vicidial_campaigns.no_hopper_leads_logins',
+            'type'  => 'string',
+        ],
+        'lead_filter'     => [
+            'field' => 'vicidial_campaigns.lead_filter_id',
+            'type'  => 'string',
+        ],
+        'dial_method'     => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'list_order'      => [
+            'field' => 'vicidial_campaigns.lead_order',
+            'type'  => 'string',
+        ],
+    ],
+    'list'     => [
+        'name'                 => [
+            'field' => 'vicidial_lists.list_name',
+            'type'  => 'string',
+        ],
+        'id'                   => [
+            'field' => 'vicidial_lists.list_id',
+            'type'  => 'string',
+        ],
+        'status'               => [
+            'field' => 'vicidial_lists.active',
+            'type'  => 'string',
+        ],
+        'expiration_date'      => [
+            'field' => 'vicidial_lists.expiration_date',
+            'type'  => 'string',
+        ],
+        'total_number_records' => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+        'duplicate_records'    => [
+            'field' => '',
+            'type'  => 'string',
+        ],
+    ],
+];
